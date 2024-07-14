c5ea9abda4f0a27cbd0647d9fc2c2f1caf33776f
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-07-14 16:44:34 +0900

    chore: 백엔드 초기 개발 환경 설정

---
+++ backend/src/main/java/com/woowacourse/momo/MomoApplication.java
@@ -0,0 +1,12 @@
+package com.woowacourse.momo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomoApplication.class, args);
+    }
+}
