9508c7636f9efd6ba65a312d39f0f9dc168b1a02
hwinkr <dnddl8280@naver.com>
2024-07-15 15:27:58 +0900

    config: babel을 사용하기 위한 설정 추가

---
+++ frontend/babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    ['@babel/preset-typescript'],
+  ],
+};
