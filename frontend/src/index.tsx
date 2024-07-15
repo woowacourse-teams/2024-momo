43f2453362f1173b57505e5c37af90b4ef432237
hwinkr <dnddl8280@naver.com>
2024-07-15 15:22:23 +0900

    chore: import 순서 수정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import React from 'react'
-import App from './App'
-import { Global } from '@emotion/react'
-import { globalStyles } from './styles/global'
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import { Global } from '@emotion/react';
+import App from './App';
+import { globalStyles } from './styles/global';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
-)
+);
