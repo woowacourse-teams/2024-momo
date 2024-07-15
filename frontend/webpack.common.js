7c981462628251aab2d05c8babfac101d5847625
해리(최현웅) <dnddl8280@naver.com>
2024-07-15 15:53:27 +0900

    [FE] 프론트엔드 기본 환경 세팅 (#5)
    * chore: src 내 폴더 구조 세팅
    
    Co-authored-by: 낙타/송재석 <Largopie@users.noreply.github.com>
    Co-authored-by: Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
    
    * chore: 프로젝트 초기 환경 설정(React, TypeScript, Webpack, ESLint, Prettier, Stylelint)
    
    Co-authored-by: 낙타/송재석 <Largopie@users.noreply.github.com>
    Co-authored-by: Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
    
    * chore: .vscode 설정 및 사용하지 않는 파일 삭제
    
    Co-authored-by: 낙타/송재석 <Largopie@users.noreply.github.com>
    Co-authored-by: Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
    
    * chore: index.html 템플릿 추가
    
    Co-authored-by: 낙타/송재석 <Largopie@users.noreply.github.com>
    Co-authored-by: Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
    
    * chore: emotion을 사용한 글로벌 스타일 적용
    
    Co-authored-by: 낙타/송재석 <Largopie@users.noreply.github.com>
    Co-authored-by: Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>
    
    * chore: type 폴더 추가
    
    * chore: import 순서 수정
    
    * feat: 최상위 App 컴포넌트 구현
    
    * refactor: ts-loader에서 babel-loader를 사용하는 것으로 수정, 세미콜론 추가
    
    * config: 컴파일 시 주석을 제거하도록 하는 설정 추가, 타입 체커 용도로만 사용하도록 설정 변경
    
    * config: ts-loader 의존성 제거, babel-loader 의존성 추가
    
    * config: babel을 사용하기 위한 설정 추가
    
    * config: ESLint 설정 변경
    
    ---------
    
    Co-authored-by: 낙타/송재석 <Largopie@users.noreply.github.com>
    Co-authored-by: Yoonkyoung (빙봉) <dev.yoonkyoung@gmail.com>

---
+++ frontend/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = () => ({
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'momo-bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+});
