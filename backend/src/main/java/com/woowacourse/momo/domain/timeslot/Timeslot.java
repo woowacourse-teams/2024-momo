485e25e7a60da07dc2f09387cda6070cd964c2df
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-07-16 10:01:31 +0900

    [BE] 타임슬롯 도메인 수정 (#19)
    타임슬롯 도메인을 테이블에서 enum 타입으로 수정

--- backend/src/main/java/com/woowacourse/momo/domain/timeslot/Timeslot.java
+++ backend/src/main/java/com/woowacourse/momo/domain/timeslot/Timeslot.java
@@ -1,29 +1,61 @@
 package com.woowacourse.momo.domain.timeslot;
 
-import com.woowacourse.momo.domain.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.time.LocalTime;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Table(name = "timeslot")
-@Entity
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Timeslot extends BaseEntity {
+public enum Timeslot {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    TIME_0000(LocalTime.of(0, 0)),
+    TIME_0030(LocalTime.of(0, 30)),
+    TIME_0100(LocalTime.of(1, 0)),
+    TIME_0130(LocalTime.of(1, 30)),
+    TIME_0200(LocalTime.of(2, 0)),
+    TIME_0230(LocalTime.of(2, 30)),
+    TIME_0300(LocalTime.of(3, 0)),
+    TIME_0330(LocalTime.of(3, 30)),
+    TIME_0400(LocalTime.of(4, 0)),
+    TIME_0430(LocalTime.of(4, 30)),
+    TIME_0500(LocalTime.of(5, 0)),
+    TIME_0530(LocalTime.of(5, 30)),
+    TIME_0600(LocalTime.of(6, 0)),
+    TIME_0630(LocalTime.of(6, 30)),
+    TIME_0700(LocalTime.of(7, 0)),
+    TIME_0730(LocalTime.of(7, 30)),
+    TIME_0800(LocalTime.of(8, 0)),
+    TIME_0830(LocalTime.of(8, 30)),
+    TIME_0900(LocalTime.of(9, 0)),
+    TIME_0930(LocalTime.of(9, 30)),
+    TIME_1000(LocalTime.of(10, 0)),
+    TIME_1030(LocalTime.of(10, 30)),
+    TIME_1100(LocalTime.of(11, 0)),
+    TIME_1130(LocalTime.of(11, 30)),
+    TIME_1200(LocalTime.of(12, 0)),
+    TIME_1230(LocalTime.of(12, 30)),
+    TIME_1300(LocalTime.of(13, 0)),
+    TIME_1330(LocalTime.of(13, 30)),
+    TIME_1400(LocalTime.of(14, 0)),
+    TIME_1430(LocalTime.of(14, 30)),
+    TIME_1500(LocalTime.of(15, 0)),
+    TIME_1530(LocalTime.of(15, 30)),
+    TIME_1600(LocalTime.of(16, 0)),
+    TIME_1630(LocalTime.of(16, 30)),
+    TIME_1700(LocalTime.of(17, 0)),
+    TIME_1730(LocalTime.of(17, 30)),
+    TIME_1800(LocalTime.of(18, 0)),
+    TIME_1830(LocalTime.of(18, 30)),
+    TIME_1900(LocalTime.of(19, 0)),
+    TIME_1930(LocalTime.of(19, 30)),
+    TIME_2000(LocalTime.of(20, 0)),
+    TIME_2030(LocalTime.of(20, 30)),
+    TIME_2100(LocalTime.of(21, 0)),
+    TIME_2130(LocalTime.of(21, 30)),
+    TIME_2200(LocalTime.of(22, 0)),
+    TIME_2230(LocalTime.of(22, 30)),
+    TIME_2300(LocalTime.of(23, 0)),
+    TIME_2330(LocalTime.of(23, 30));
 
-    @Column(nullable = false)
-    private LocalTime startedAt;
+    private final LocalTime time;
+
+    Timeslot(LocalTime time) {
+        this.time = time;
+    }
 }
