3d08538c360bb3bbca3d5feda3fd2609f4e88e8e
hwinkr <dnddl8280@naver.com>
2024-07-17 18:09:05 +0900

    feat: 라우팅 설정 및 쿼리, 테마 프로바이더 적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,21 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
-import { Global } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
+
 import App from './App';
-import { globalStyles } from './styles/global';
+import globalStyles from './styles/global';
+import theme from './styles/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Global styles={globalStyles} />
-    <App />
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
