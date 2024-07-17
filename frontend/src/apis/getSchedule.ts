d041958b37b44bea212b7ef03f44f7bd9c8d1ffb
hwinkr <dnddl8280@naver.com>
2024-07-17 16:48:03 +0900

    feat: 가능한 시간을 선택, 수정, 조회할 수 있는 api 함수 구현
    - API_URL 상수화 및 환경 변수로 관리

---
+++ frontend/src/apis/getSchedule.ts
@@ -0,0 +1,21 @@
+import { API_URL } from '../constants/api';
+
+const getSchedule = async () => {
+  const url = `${API_URL}/api/v1/schedule}`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data;
+};
+
+export default getSchedule;
