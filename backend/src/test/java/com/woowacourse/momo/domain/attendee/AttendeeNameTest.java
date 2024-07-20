55cde03e1ce8f67d51efc1498bb1d453cd820eca
Seokmyung Ham <gkatjraud1@gmail.com>
2024-07-20 13:20:41 +0900

    style: 테스트 어노테이션 순서 변경
    Co-authored-by: ikjo39 <chi4321@naver.com>

--- backend/src/test/java/com/woowacourse/momo/domain/attendee/AttendeeNameTest.java
+++ backend/src/test/java/com/woowacourse/momo/domain/attendee/AttendeeNameTest.java
@@ -8,15 +8,15 @@ import org.junit.jupiter.api.Test;
 
 class AttendeeNameTest {
 
-    @Test
     @DisplayName("참여자 이름이 20글자를 초과하면 예외를 발생시킨다.")
+    @Test
     void throwsExceptionIfAttendeeNameIsTooLong() {
         assertThatThrownBy(() -> new AttendeeName("woowacourse-momo-jjangjjanng-momo"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
     @DisplayName("참여자 이름 객체가 정상 생성된다.")
+    @Test
     void createAttendeeNameObjectSuccessfully() {
         assertThatNoException()
                 .isThrownBy(() -> new AttendeeName("momo"));
--- backend/src/test/java/com/woowacourse/momo/domain/attendee/AttendeePasswordTest.java
+++ backend/src/test/java/com/woowacourse/momo/domain/attendee/AttendeePasswordTest.java
@@ -8,15 +8,15 @@ import org.junit.jupiter.api.Test;
 
 class AttendeePasswordTest {
 
-    @Test
     @DisplayName("참가자 비밀번호가 20글자를 초과하면 예외를 발생시킨다.")
+    @Test
     void throwsExceptionIfAttendeePasswordIsTooLong() {
         assertThatThrownBy(() -> new AttendeePassword("woowacourse-momo-jjangjjanng-momo"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
     @DisplayName("참가자 비밀번호 객체가 정상 생성된다.")
+    @Test
     void createAttendeePasswordObjectSuccessfully() {
         assertThatNoException()
                 .isThrownBy(() -> new AttendeePassword("momo"));
