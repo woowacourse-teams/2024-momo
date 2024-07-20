49769aa60420c5f28a858b61beaa188c4324dcfa
Seokmyung Ham <gkatjraud1@gmail.com>
2024-07-20 18:08:39 +0900

    fix(AttendeeController): 누락된 @Valid 어노테이션 추가
    Co-authored-by: ikjo39 <chi4321@naver.com>

--- backend/src/main/java/com/woowacourse/momo/controller/attendee/AttendeeController.java
+++ backend/src/main/java/com/woowacourse/momo/controller/attendee/AttendeeController.java
@@ -4,6 +4,7 @@ import com.woowacourse.momo.controller.MomoApiResponse;
 import com.woowacourse.momo.service.attendee.AttendeeService;
 import com.woowacourse.momo.service.attendee.dto.AttendeeLoginRequest;
 import com.woowacourse.momo.service.attendee.dto.TokenResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,9 @@ public class AttendeeController {
     private final AttendeeService attendeeService;
 
     @PostMapping("/api/v1/login/{uuid}")
-    public MomoApiResponse<TokenResponse> login(@PathVariable String uuid, @RequestBody AttendeeLoginRequest request) {
+    public MomoApiResponse<TokenResponse> login(
+            @PathVariable String uuid, @Valid @RequestBody AttendeeLoginRequest request
+    ) {
         return new MomoApiResponse<>(attendeeService.login(uuid, request));
     }
 }
