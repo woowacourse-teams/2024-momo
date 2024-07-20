6b98ef436983e18814491d2a6256686c9f2a1004
Seokmyung Ham <gkatjraud1@gmail.com>
2024-07-20 15:47:24 +0900

    test(DatabaseCleanupListener): 테스트 시 데이터베이스 격리를 위한 커스텀 리스너 생성
    Co-authored-by: ikjo39 <chi4321@naver.com>

---
+++ backend/src/test/java/com/woowacourse/momo/support/DatabaseCleanupListener.java
@@ -0,0 +1,36 @@
+package com.woowacourse.momo.support;
+
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
+
+public class DatabaseCleanupListener extends AbstractTestExecutionListener {
+
+    private final String sql = """
+            SELECT CONCAT('TRUNCATE TABLE ', TABLE_NAME, ' RESTART IDENTITY;')
+            FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_SCHEMA = 'PUBLIC';
+            """;
+
+    @Override
+    public void afterTestMethod(TestContext testContext) {
+        JdbcTemplate jdbcTemplate = getJdbcTemplateBean(testContext);
+        List<String> queries = getTruncateQueries(jdbcTemplate);
+        execute(queries, jdbcTemplate);
+    }
+
+    private JdbcTemplate getJdbcTemplateBean(TestContext testContext) {
+        return testContext.getApplicationContext().getBean(JdbcTemplate.class);
+    }
+
+    private List<String> getTruncateQueries(JdbcTemplate jdbcTemplate) {
+        return jdbcTemplate.queryForList(sql, String.class);
+    }
+
+    private void execute(List<String> queries, JdbcTemplate jdbcTemplate) {
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY FALSE;");
+        queries.forEach(jdbcTemplate::execute);
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY TRUE;");
+    }
+}
