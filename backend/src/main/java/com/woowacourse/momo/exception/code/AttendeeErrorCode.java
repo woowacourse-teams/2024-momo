8ad0fb1f3ae44431859e4daa5e6ba2625bd044b0
Seokmyung Ham <gkatjraud1@gmail.com>
2024-07-22 01:05:15 +0900

    refactor(MeetingErrorCode): uuid 에러 코드를 MeetingErrorCode 로 이동

--- backend/src/main/java/com/woowacourse/momo/exception/code/AttendeeErrorCode.java
+++ backend/src/main/java/com/woowacourse/momo/exception/code/AttendeeErrorCode.java
@@ -6,7 +6,6 @@ public enum AttendeeErrorCode implements ErrorCodeType {
 
     INVALID_NAME_LENGTH(HttpStatus.BAD_REQUEST, "이름 길이는 최대 20글자까지 가능합니다."),
     INVALID_PASSWORD_LENGTH(HttpStatus.BAD_REQUEST, "비밀번호 길이는 최대 20글자까지 가능합니다."),
-    INVALID_UUID(HttpStatus.BAD_REQUEST, "유효하지 않은 UUID 입니다."),
     PASSWORD_MISMATCHED(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
 
     private final HttpStatus httpStatus;
