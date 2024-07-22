a36d133230e7ef0ef4c0e5cdb08555bdf7c6a568
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-07-22 14:51:57 +0900

    design: font-face 정의 추가

---
+++ frontend/src/styles/fonts.ts
@@ -0,0 +1,33 @@
+import { css } from '@emotion/react';
+
+export const fontFaces = css`
+  @font-face {
+    font-family: 'Spoqa Han Sans Neo';
+    font-weight: 100;
+    src: url('../assets/fonts/SpoqaHanSansNeo-Thin.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Spoqa Han Sans Neo';
+    font-weight: 300;
+    src: url('../assets/fonts/SpoqaHanSansNeo-Light.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Spoqa Han Sans Neo';
+    font-weight: 400;
+    src: url('../assets/fonts/SpoqaHanSansNeo-Regular.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Spoqa Han Sans Neo';
+    font-weight: 500;
+    src: url('../assets/fonts/SpoqaHanSansNeo-Medium.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Spoqa Han Sans Neo';
+    font-weight: 700;
+    src: url('../assets/fonts/SpoqaHanSansNeo-Bold.woff2') format('woff2');
+  }
+`;
