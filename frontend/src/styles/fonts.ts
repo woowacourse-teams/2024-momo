ae3a348b531e6fa196c1d0bbb38be546d8dcafeb
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-07-22 22:29:38 +0900

    fix: 웹팩 폰트 로딩 경로 수정
    웹팩 번들링 과정에서 폰트 파일 경로 문제를 해결하기 위해 require를 사용하여 폰트 파일을 로드하도록 수정

--- frontend/src/styles/fonts.ts
+++ frontend/src/styles/fonts.ts
@@ -4,30 +4,30 @@ export const fontFaces = css`
   @font-face {
     font-family: 'Spoqa Han Sans Neo';
     font-weight: 100;
-    src: url('../assets/fonts/SpoqaHanSansNeo-Thin.woff2') format('woff2');
+    src: url(${require('../assets/fonts/SpoqaHanSansNeo-Thin.woff2')}) format('woff2');
   }
 
   @font-face {
     font-family: 'Spoqa Han Sans Neo';
     font-weight: 300;
-    src: url('../assets/fonts/SpoqaHanSansNeo-Light.woff2') format('woff2');
+    src: url(${require('../assets/fonts/SpoqaHanSansNeo-Light.woff2')}) format('woff2');
   }
 
   @font-face {
     font-family: 'Spoqa Han Sans Neo';
     font-weight: 400;
-    src: url('../assets/fonts/SpoqaHanSansNeo-Regular.woff2') format('woff2');
+    src: url(${require('../assets/fonts/SpoqaHanSansNeo-Regular.woff2')}) format('woff2');
   }
 
   @font-face {
     font-family: 'Spoqa Han Sans Neo';
     font-weight: 500;
-    src: url('../assets/fonts/SpoqaHanSansNeo-Medium.woff2') format('woff2');
+    src: url(${require('../assets/fonts/SpoqaHanSansNeo-Medium.woff2')}) format('woff2');
   }
 
   @font-face {
     font-family: 'Spoqa Han Sans Neo';
     font-weight: 700;
-    src: url('../assets/fonts/SpoqaHanSansNeo-Bold.woff2') format('woff2');
+    src: url(${require('../assets/fonts/SpoqaHanSansNeo-Bold.woff2')}) format('woff2');
   }
 `;
