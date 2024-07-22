dec3d79388c4341ffe0acedf34df831eebe65615
Largopie <106071687+Largopie@users.noreply.github.com>
2024-07-22 15:44:37 +0900

    design(TimeSlot): 스타일 로직 통합

--- frontend/src/components/_common/TimeSlot/TimeSlot.styles.ts
+++ frontend/src/components/_common/TimeSlot/TimeSlot.styles.ts
@@ -3,6 +3,9 @@ import { css } from '@emotion/react';
 import theme from '@styles/theme';
 
 export const styledTd = (isSelected: boolean, isUpdate: boolean) => css`
+  width: 4rem;
+  height: 4rem;
+
   cursor: pointer;
   color: ${isSelected ? '#121010' : '#fff'};
   background: ${isSelected ? theme.linear.selectedTime : '#ececec'};
--- frontend/src/components/_common/TimeSlot/index.tsx
+++ frontend/src/components/_common/TimeSlot/index.tsx
@@ -1,5 +1,3 @@
-import { tableTexture } from '@components/TimePicker/TimePicker.styles';
-
 import { styledTd } from './TimeSlot.styles';
 
 interface TimeSlotProps {
@@ -8,5 +6,5 @@ interface TimeSlotProps {
 }
 
 export default function TimeSlot({ isSelected, isUpdate }: TimeSlotProps) {
-  return <td css={[tableTexture, styledTd(isSelected, isUpdate)]} />;
+  return <td css={styledTd(isSelected, isUpdate)} />;
 }
