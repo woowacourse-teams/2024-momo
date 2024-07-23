7639820567db112aa96bb2cef8e6878118d180da
Largopie <106071687+Largopie@users.noreply.github.com>
2024-07-23 22:37:53 +0900

    refactor: 상수 정의 as const 사용

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,3 +1,3 @@
 export const API_URL = process.env.API_URL;
 
-export const END_POINTS = {};
+export const END_POINTS = {} as const;
--- frontend/src/constants/queryKeys.ts
+++ frontend/src/constants/queryKeys.ts
@@ -1,3 +1,3 @@
 export const QUERY_KEY = {
   meeting: 'meeting',
-};
+} as const;
