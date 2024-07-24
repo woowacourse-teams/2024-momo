c369898482912c9b012ed88ac3c82deeae0b4888
Largopie <106071687+Largopie@users.noreply.github.com>
2024-07-25 00:30:33 +0900

    test(Dropdown): 컴포넌트 UI 테스트

---
+++ frontend/src/components/_common/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Dropdown from '.';
+
+const meta = {
+  title: 'Components/Dropdown',
+  component: Dropdown,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    options: {
+      control: 'object',
+      description: '드롭다운 목록에 추가할 `value`와 `label`을 담고있는 객체 배열입니다.',
+    },
+  },
+} satisfies Meta<typeof Dropdown>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    options: [
+      {
+        value: '옵션 1',
+        label: '옵션 1',
+      },
+      {
+        value: '옵션 2',
+        label: '옵션 2',
+      },
+      {
+        value: '옵션 3',
+        label: '옵션 3',
+      },
+      {
+        value: '옵션 4',
+        label: '옵션 4',
+      },
+      {
+        value: '옵션 5',
+        label: '옵션 5',
+      },
+    ],
+  },
+};
