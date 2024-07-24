de8b0a2191414fbd39fc62c1e95a56bc4c995302
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-07-25 01:57:00 +0900

    refactor: Field 컴포넌트에서 labelText와 id를 분리하여 명확히 정의
    - `label` prop을 `labelText`로 변경
    - `Field` 컴포넌트에서 `labelText`와 `id`를 명시적으로 받아서 `label`과 `Input`의 `id`에 적용하도록 수정
    -`InputHTMLAttributes<HTMLInputElement>`를 상속받아 추가적인 `input` 속성을 직접 전달할 수 있도록 개선

--- frontend/src/components/_common/Field/index.tsx
+++ frontend/src/components/_common/Field/index.tsx
@@ -3,20 +3,19 @@ import type { InputHTMLAttributes } from 'react';
 import Input from '../Input';
 import { s_description, s_field, s_label } from './Field.styles';
 
-interface FieldProps {
-  label: string;
+interface FieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  labelText: string;
   description?: string;
-  inputProps?: InputHTMLAttributes<HTMLInputElement>;
 }
 
-export default function Field({ label, description = '', inputProps }: FieldProps) {
+export default function Field({ labelText, id, description = '', ...props }: FieldProps) {
   return (
     <div css={s_field}>
-      <label css={s_label} htmlFor={label}>
-        {label}
+      <label css={s_label} htmlFor={id}>
+        {labelText}
       </label>
       {description && <div css={s_description}>{description}</div>}
-      <Input id={label} {...inputProps} />
+      <Input id={id} {...props} />
     </div>
   );
 }
