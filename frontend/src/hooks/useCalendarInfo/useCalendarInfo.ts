4d5bc7bfed556841986590e11d2fc708d560c453
hwinkr <dnddl8280@naver.com>
2024-07-24 16:53:17 +0900

    feat(useCalendarInfo): 날짜 정보를 알려주는 커스텀 훅 구현
    - year, month를 상태로 관리하여 값이 변경될 때마다 날짜 정보를 계산해서 반환
    - 달(Month)를 이동할 수 있는 함수 구현
    - 12월에서 다음을 클릭할 경우 해(Year)가 변경되도록 구현

---
+++ frontend/src/hooks/useCalendarInfo/useCalendarInfo.ts
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+import { getDayInfo, getYearMonthInfo } from './useCalendarInfo.utils';
+
+export default function useCalendarInfo() {
+  // TODO : L7 ~ L9 getCurrentDate 함수로 추상화 예정(@해리)
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+
+  const [year, setYear] = useState(currentYear);
+  const [month, setMonth] = useState(currentMonth);
+
+  const { firstDayIndex, daySlotCount } = getYearMonthInfo(year, month);
+
+  const handleGetDayInfo = (index: number) => {
+    return getDayInfo(year, month, firstDayIndex, index, currentDate);
+  };
+
+  const handlePrevMonth = () => {
+    // TODO : isCurrentDate 함수로 추상화(@해리)
+    if (year === currentYear && month === currentMonth) return;
+
+    if (month === 1) {
+      setYear(year - 1);
+      setMonth(12); // TODO : 상수화(@해리)
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
+  const handleNextMonth = () => {
+    if (month === 12) {
+      setYear(year + 1);
+      setMonth(1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+
+  return {
+    yearMonthInfo: {
+      year,
+      month,
+      daySlotCount,
+    },
+    handleGetDayInfo,
+    handlePrevMonth,
+    handleNextMonth,
+  } as const;
+}
---
+++ frontend/src/hooks/useCalendarInfo/useCalendarInfo.utils.ts
@@ -0,0 +1,38 @@
+export function getYearMonthInfo(year: number, month: number) {
+  const startDate = new Date(year, month - 1, 1);
+
+  /* 
+   로직 설명(@해리)
+   - 월요일을 index 0으로 변경하기 위해서 나머지 연산자를 활용한다.
+   - 자바스크립트 데이트 객체는 기본적으로 일요일이 인덱스가 0인데, 모모 달력은 월요일을 인덱스를 0으로 만들어줘야 한다.
+   - 따라서, 특정 달의 시작 날짜에 대한 인덱스에 6을 더해주고 7로 나눈 나머지를 사용하는 것으로 구현했다.
+  */
+  const firstDayIndex = (startDate.getDay() + 6) % 7;
+
+  const endDate = new Date(year, month, 0);
+  const lastDate = endDate.getDate();
+
+  const daySlotCount = firstDayIndex + lastDate;
+
+  return { year, month, firstDayIndex, daySlotCount } as const;
+}
+
+export function getDayInfo(
+  year: number,
+  month: number,
+  firstDayIndex: number,
+  index: number,
+  currentDate: Date,
+) {
+  const date = index - firstDayIndex + 1;
+  const isDate = index >= firstDayIndex;
+
+  const dateString = `${year}-${month}-${date}`;
+  const todayString = `${year}-${month}-${currentDate.getDate()}`;
+  const isToday = dateString === todayString;
+  // TODO : 일단은 일요일만 isHolday로 설정되도록 구현, 추후에 진짜 공휴일도 포함할 것인지 논의 필요, 찾아보니 라이브러리가 있더라구요.(@해리)
+  // TODO : 매직넘버 의미있는 상수화 필요(@해리)
+  const isHoliday = index % 7 === 6;
+
+  return { date, dateString, isDate, isToday, isHoliday } as const;
+}
