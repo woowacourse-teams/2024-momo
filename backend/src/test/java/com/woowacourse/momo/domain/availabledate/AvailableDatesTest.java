7216cf9e3c872411d10e3ce63928fe99085388e8
Seokmyung Ham <gkatjraud1@gmail.com>
2024-07-24 16:21:04 +0900

    test(AvailableDatesTest): findByDate() 테스트 추가

--- backend/src/test/java/com/woowacourse/momo/domain/availabledate/AvailableDatesTest.java
+++ backend/src/test/java/com/woowacourse/momo/domain/availabledate/AvailableDatesTest.java
@@ -1,18 +1,54 @@
 package com.woowacourse.momo.domain.availabledate;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.woowacourse.momo.domain.meeting.Meeting;
 import com.woowacourse.momo.domain.meeting.Meeting;
 import com.woowacourse.momo.exception.MomoException;
 import com.woowacourse.momo.exception.code.AvailableDateErrorCode;
 import com.woowacourse.momo.fixture.MeetingFixture;
 import java.time.LocalDate;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AvailableDatesTest {
+
+    private AvailableDates availableDates;
+    private LocalDate tomorrow;
+
+    @BeforeEach
+    void setUp() {
+        LocalDate today = LocalDate.now();
+        tomorrow = today.plusDays(1);
+
+        availableDates = new AvailableDates(
+                List.of(new AvailableDate(today, MeetingFixture.GAME.create()),
+                        new AvailableDate(tomorrow, MeetingFixture.GAME.create()))
+        );
+    }
+
+    @DisplayName("주어진 날짜와 일치하는 가능 날짜를 찾고, 없으면 예외를 발생시킨다.")
+    @Test
+    void throwsExceptionIfDateNotFound() {
+        LocalDate other = tomorrow.plusDays(1);
+
+        assertThatThrownBy(() -> availableDates.findByDate(other))
+                .isInstanceOf(MomoException.class)
+                .hasMessage(AvailableDateErrorCode.INVALID_AVAILABLE_DATE.message());
+    }
+
+    @DisplayName("주어진 날짜와 일치하는 가능 날짜를 찾고, 존재하면 가능 날짜 객체를 반환한다.")
+    @Test
+    void successfulWhenDateIsFound() {
+        AvailableDate availableDate = availableDates.findByDate(tomorrow);
+
+        assertThat(availableDate.getDate()).isEqualTo(tomorrow);
+    }
 
     @DisplayName("가능한 시간의 값이 중복이면 예외가 발생한다.")
     @Test
