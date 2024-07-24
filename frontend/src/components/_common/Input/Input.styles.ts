ffd1e784181e9ec6cabbf305c7f7eb2228b7e425
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-07-24 15:40:40 +0900

    design: Input 컴포넌트 스타일 정의 및 적용

---
+++ frontend/src/components/_common/Input/Input.styles.ts
@@ -0,0 +1,8 @@
+import { css } from '@emotion/react';
+
+export const s_input = css`
+  box-sizing: border-box;
+  width: 20rem;
+  height: 2.5rem;
+  padding: 0 0.5rem;
+`;
--- frontend/src/components/_common/Input/index.tsx
+++ frontend/src/components/_common/Input/index.tsx
@@ -1,7 +1,9 @@
 import type { InputHTMLAttributes } from 'react';
 
+import { s_input } from './Input.styles';
+
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export default function Input({ type = 'text', placeholder = '', ...props }: InputProps) {
-  return <input type={type} placeholder={placeholder} {...props} />;
+  return <input css={s_input} type={type} placeholder={placeholder} {...props} />;
 }
