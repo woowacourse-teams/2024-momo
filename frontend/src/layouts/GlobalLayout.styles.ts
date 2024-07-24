83395601b39dbd7c59b1cc5c13b42f3814913de0
Largopie <106071687+Largopie@users.noreply.github.com>
2024-07-24 14:51:15 +0900

    feat: Header 컴포넌트 GlobalLayout에 적용

--- frontend/src/layouts/GlobalLayout.styles.ts
+++ frontend/src/layouts/GlobalLayout.styles.ts
@@ -4,10 +4,11 @@ export const s_globalContainer = css`
   min-width: 39.3rem;
   max-width: 60rem;
   height: 100%;
-  padding: 1.2rem;
 
   background-color: #fff;
   box-shadow: rgb(100 100 111 / 20%) 0 7px 29px 0;
 `;
 
-export const a = 10;
+export const s_content = css`
+  padding: 1.6rem;
+`;
