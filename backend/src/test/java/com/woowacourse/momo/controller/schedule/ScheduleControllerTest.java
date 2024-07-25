3daa9671611599d898a06e4e45c7b13e6b079e90
ikjo39 <chi4321@naver.com>
2024-07-25 21:48:14 +0900

    refactor: 공통된 테스트 데이터 `@BeforeEach` 내 통합

--- backend/src/test/java/com/woowacourse/momo/controller/schedule/ScheduleControllerTest.java
+++ backend/src/test/java/com/woowacourse/momo/controller/schedule/ScheduleControllerTest.java
@@ -10,7 +10,6 @@ import com.woowacourse.momo.domain.meeting.MeetingRepository;
 import com.woowacourse.momo.domain.schedule.Schedule;
 import com.woowacourse.momo.domain.schedule.ScheduleRepository;
 import com.woowacourse.momo.domain.timeslot.Timeslot;
-import com.woowacourse.momo.fixture.AttendeeFixture;
 import com.woowacourse.momo.fixture.MeetingFixture;
 import com.woowacourse.momo.service.schedule.dto.DateTimesCreateRequest;
 import com.woowacourse.momo.service.schedule.dto.ScheduleCreateRequest;
@@ -49,19 +48,23 @@ class ScheduleControllerTest {
     @Autowired
     private ScheduleRepository scheduleRepository;
 
+    private Meeting meeting;
+    private Attendee attendee;
+    private AvailableDate today;
+    private AvailableDate tomorrow;
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+        meeting = meetingRepository.save(MeetingFixture.MOVIE.create());
+        attendee = attendeeRepository.save(new Attendee(meeting, "name", "password", Role.GUEST));
+        today = availableDateRepository.save(new AvailableDate(LocalDate.now(), meeting));
+        tomorrow = availableDateRepository.save(new AvailableDate(LocalDate.now().plusDays(1), meeting));
     }
 
     @DisplayName("참가자가 스케줄을 생성하는데 성공하면 200 상태 코드를 응답한다.")
     @Test
     void create() {
-        Meeting meeting = meetingRepository.save(MeetingFixture.MOVIE.create());
-        Attendee attendee = attendeeRepository.save(new Attendee(meeting, "name", "password", Role.GUEST));
-        AvailableDate today = availableDateRepository.save(new AvailableDate(LocalDate.now(), meeting));
-        AvailableDate tomorrow = availableDateRepository.save(new AvailableDate(LocalDate.now().plusDays(1), meeting));
-
         List<LocalTime> times = List.of(Timeslot.TIME_0100.getLocalTime(), Timeslot.TIME_0130.getLocalTime());
         List<DateTimesCreateRequest> dateTimes = List.of(
                 new DateTimesCreateRequest(today.getDate(), times),
@@ -78,16 +81,9 @@ class ScheduleControllerTest {
                 .statusCode(HttpStatus.OK.value());
     }
 
-    @DisplayName("약속 uuid와 참가자 이름으로 스케줄 조회를 요쳥하면 200OK를 반환한다.")
+    @DisplayName("약속 uuid와 참가자 이름으로 스케줄 조회를 요쳥하면 200 상태 코드를 응답한다.")
     @Test
     void findSingleSchedule() {
-        Meeting meeting = meetingRepository.save(MeetingFixture.GAME.create());
-        attendeeRepository.save(AttendeeFixture.HOST_JAZZ.create(meeting));
-        Attendee attendee = attendeeRepository.save(AttendeeFixture.GUEST_DAON.create(meeting));
-        LocalDate now = LocalDate.now();
-        AvailableDate today = availableDateRepository.save(new AvailableDate(null, now, meeting));
-        AvailableDate tomorrow = availableDateRepository.save(new AvailableDate(null, now.plusDays(1), meeting));
-        availableDateRepository.save(new AvailableDate(null, now.plusDays(2), meeting));
         List<Schedule> schedules = new ArrayList<>();
         schedules.add(new Schedule(attendee, today, Timeslot.TIME_0300));
         schedules.add(new Schedule(attendee, today, Timeslot.TIME_0400));
