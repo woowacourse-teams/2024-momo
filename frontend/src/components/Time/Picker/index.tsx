2c1c486299ba079e6a15a8fdebb4c9dd187bed4c
Largopie <106071687+Largopie@users.noreply.github.com>
2024-07-25 21:47:10 +0900

    refactor: schedule 관련 서버 상태 관리 로직 폴더 분리

--- frontend/src/components/Time/Picker/index.tsx
+++ frontend/src/components/Time/Picker/index.tsx
@@ -9,7 +9,7 @@ import useTimePick from '@hooks/useTimePick/useTimePick';
 
 import type { GetMeetingResponse } from '@apis/meetings';
 
-import { usePostScheduleMutation } from '@stores/servers/meeting/mutations';
+import { usePostScheduleMutation } from '@stores/servers/schedule/mutations';
 
 import { s_buttonContainer, s_cell, s_table, s_th } from '../Time.styles';
 import { convertToSchedule, generateScheduleMatrix } from './TimePicker.util';
