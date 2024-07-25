9d197412ecdf8e8ac0b4148a7b4a92e289d07623
Largopie <106071687+Largopie@users.noreply.github.com>
2024-07-26 01:42:02 +0900

    refactor: router 경로 수정

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -5,30 +5,38 @@ import GlobalLayout from '@layouts/GlobalLayout';
 import { TimePickerUpdateStateProvider } from '@contexts/TimePickerUpdateStateProvider';
 
 import CreateMeetingPage from '@pages/CreateMeetingPage';
+import MeetingLinkSharePage from '@pages/MeetingLinkSharePage';
 import MeetingTimePickPage from '@pages/MeetingTimePickPage';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <GlobalLayout />,
-      children: [
-        {
-          index: true,
-          element: (
-            <TimePickerUpdateStateProvider>
-              <MeetingTimePickPage />
-            </TimePickerUpdateStateProvider>
-          ),
-        },
-        {
-          path: 'create-meeting',
-          element: <CreateMeetingPage />,
-        },
-      ],
-    },
-  ],
-  {},
-);
+// TODO: 추후 라우팅 경로 다시 한 번 수정해야 함(@낙타)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <GlobalLayout />,
+    children: [
+      {
+        path: 'meeting',
+        children: [
+          {
+            path: ':uuid',
+            element: (
+              <TimePickerUpdateStateProvider>
+                <MeetingTimePickPage />
+              </TimePickerUpdateStateProvider>
+            ),
+          },
+          {
+            path: 'create',
+            element: <CreateMeetingPage />,
+          },
+          {
+            path: 'complete',
+            element: <MeetingLinkSharePage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 export default router;
