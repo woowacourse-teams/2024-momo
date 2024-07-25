16d4cf1577813e74c79f8092dfb3be84ba8c6bb8
낙타/송재석 <106071687+Largopie@users.noreply.github.com>
2024-07-25 11:17:21 +0900

    [FE] 헤더 컴포넌트 구현 (#63)
    * fix: 잘못된 assets 경로 수정
    
    * chore: 로고, 메뉴 svg 이미지 추가
    
    * feat(Header): 컴포넌트 구현
    
    * feat: Header 컴포넌트 GlobalLayout에 적용
    
    * design: 로고 높이 중앙 정렬
    
    * design: 좌우 padding만 적용되도록 수정

---
+++ frontend/src/components/_common/Header/Header.styles.ts
@@ -0,0 +1,26 @@
+import { css } from '@emotion/react';
+
+export const s_header = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  height: 6rem;
+  padding: 0 1.6rem;
+
+  box-shadow: 0 4px 4px rgb(0 0 0 / 25%);
+`;
+
+export const s_logoContainer = css`
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+  width: 100%;
+  height: 3.6rem;
+`;
+
+export const s_title = css`
+  font-size: 2.4rem;
+  font-weight: 800;
+`;
