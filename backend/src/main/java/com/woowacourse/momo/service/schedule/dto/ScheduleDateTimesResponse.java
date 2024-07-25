6d9a18a40a63298553724476ae731e84fc5e2b46
Jo Hyeong-Ik <70360890+ikjo39@users.noreply.github.com>
2024-07-26 00:54:47 +0900

    [BE] 참가자 스케줄 조회 반환 값 내 시간 포멧팅 수정 (#86)
    refactor(ScheduleDateTimesResponse): 시간 필드 포멧팅 수정

--- backend/src/main/java/com/woowacourse/momo/service/schedule/dto/ScheduleDateTimesResponse.java
+++ backend/src/main/java/com/woowacourse/momo/service/schedule/dto/ScheduleDateTimesResponse.java
@@ -3,6 +3,8 @@ package com.woowacourse.momo.service.schedule.dto;
 import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.toList;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import com.woowacourse.momo.domain.schedule.Schedule;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -10,7 +12,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public record ScheduleDateTimesResponse(LocalDate date, List<LocalTime> times) {
+public record ScheduleDateTimesResponse(
+        LocalDate date,
+        @JsonFormat(shape = Shape.STRING, pattern = "HH:mm", timezone = "Asia/Seoul") List<LocalTime> times
+) {
 
     public static List<ScheduleDateTimesResponse> from(List<Schedule> schedules) {
         Map<LocalDate, List<LocalTime>> results = schedules.stream()
