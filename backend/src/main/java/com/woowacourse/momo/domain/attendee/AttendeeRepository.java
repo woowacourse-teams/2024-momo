070b193de977a5e762de14027fe08798bc863ad6
Seokmyung Ham <gkatjraud1@gmail.com>
2024-07-25 22:01:59 +0900

    [BE] 기존 약속 정보 조회 API 응답에 참가자들의 정보 추가 (#81)
    feat(MeetingService): 기존 약속 정보 조회 API 응답에 참가자들 정보 추가

--- backend/src/main/java/com/woowacourse/momo/domain/attendee/AttendeeRepository.java
+++ backend/src/main/java/com/woowacourse/momo/domain/attendee/AttendeeRepository.java
@@ -1,10 +1,13 @@
 package com.woowacourse.momo.domain.attendee;
 
 import com.woowacourse.momo.domain.meeting.Meeting;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttendeeRepository extends JpaRepository<Attendee, Long> {
 
     Optional<Attendee> findByMeetingAndName(Meeting meeting, AttendeeName name);
+
+    List<Attendee> findAllByMeeting(Meeting meeting);
 }
