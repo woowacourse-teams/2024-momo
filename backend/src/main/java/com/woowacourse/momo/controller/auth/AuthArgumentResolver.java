78b88ea2aa4426f806480ee4d0d6b0e1faa441f6
Seokmyung Ham <gkatjraud1@gmail.com>
2024-07-26 10:59:18 +0900

    feat(AuthArgumentResolver): ArgumentResolver 추가

---
+++ backend/src/main/java/com/woowacourse/momo/controller/auth/AuthArgumentResolver.java
@@ -0,0 +1,48 @@
+package com.woowacourse.momo.controller.auth;
+
+import com.woowacourse.momo.exception.MomoException;
+import com.woowacourse.momo.exception.code.AuthErrorCode;
+import com.woowacourse.momo.service.auth.JwtManager;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BEARER = "Bearer ";
+
+    private final JwtManager jwtManager;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthAttendee.class) &&
+                parameter.getParameterType().equals(long.class);
+    }
+
+    @Override
+    public Long resolveArgument(
+            @NonNull MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            @NonNull NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) {
+        String token = getToken(webRequest);
+        return jwtManager.extract(token);
+    }
+
+    private String getToken(NativeWebRequest webRequest) {
+        String header = webRequest.getHeader(AUTHORIZATION);
+        if (header == null) {
+            throw new MomoException(AuthErrorCode.NOT_FOUND_TOKEN);
+        }
+        return header.replaceFirst(BEARER, "");
+    }
+}
--- backend/src/main/java/com/woowacourse/momo/exception/code/AuthErrorCode.java
+++ backend/src/main/java/com/woowacourse/momo/exception/code/AuthErrorCode.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public enum AuthErrorCode implements ErrorCodeType {
 
-    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 토큰입니다.");
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 토큰입니다."),
+    NOT_FOUND_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
