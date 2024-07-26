02059abec6c8884776b72e70677a61e08c82513a
hwinkr <dnddl8280@naver.com>
2024-07-26 11:52:03 +0900

    feat: 약속 현황을 조회할 때 사용되는 컴포넌트 구현
    - uuid를 통해서 약속 현황을 가져와요 :)
    - 수정하기 버튼을 클릭하면 TimePicker 컴포넌트 렌더링을 트리거해요 :)

--- frontend/src/components/Time/Time.styles.tsx
+++ frontend/src/components/Time/Time.styles.tsx
@@ -1,40 +1,54 @@
 import { css } from '@emotion/react';
+import type { CSSProperties } from 'react';
 
-import theme from '@styles/theme';
+const COLOR_VARIANTS = {
+  1: '#FFEBE8',
+  2: '#FFD4D1',
+  3: '#FFBABC',
+  4: '#FFA9B4',
+  5: '#FF8DA6',
+  6: '#DB678B',
+  7: '#B74775',
+  8: '#932C61',
+  9: '#7A1B54',
+} as Record<number, CSSProperties['color']>;
 
 export const s_table = css`
-  cursor: pointer;
-  user-select: none;
-
-  border-spacing: 0.4rem 0.4rem;
-  border-collapse: collapse;
+  border-spacing: 0;
   border-collapse: separate;
-
   width: 100%;
 `;
 
-export const s_td = css`
-  cursor: pointer;
-  padding: 8px;
-  text-align: center;
-  border-radius: 0.4rem;
-`;
-
-export const s_selected = css`
-  color: white;
-  background: ${theme.linear.selectedTime};
-`;
-
 export const s_th = css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  padding: 10px;
+  font-weight: normal;
+  color: #666;
+  text-align: center;
 `;
 
-export const s_cell = css`
-  cursor: default;
-  width: 4rem;
-  height: 4rem;
+export const s_td = (variants: number) => css`
+  width: 100px;
+  height: 40px;
+  background-color: ${COLOR_VARIANTS[variants]};
+  border: 1px solid #eee;
+`;
+
+export const s_timeColumn = css`
+  position: relative;
+  width: 60px;
+`;
+
+export const s_timeText = css`
+  position: absolute;
+  top: -0.7rem;
+
+  padding: 0 2px;
+
+  font-size: 12px;
+  color: #888;
+  text-align: start;
+
+  background-color: white;
 `;
 
 export const s_buttonContainer = css`
