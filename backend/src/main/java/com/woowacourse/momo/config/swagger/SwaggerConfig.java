3ecb8c3742a8db039af1bc7b231fbc8e488d434c
HyungUk Ryu <hw0603@naver.com>
2024-07-30 15:39:13 +0900

    refactor(SwaggerConfig): 현재 불필요한 API 버전 필드 제거

--- backend/src/main/java/com/woowacourse/momo/config/swagger/SwaggerConfig.java
+++ backend/src/main/java/com/woowacourse/momo/config/swagger/SwaggerConfig.java
@@ -6,19 +6,12 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 public class SwaggerConfig {
-
-    private final String apiVersion;
-
-    public SwaggerConfig(@Value("${apiVersion}") String apiVersion) {
-        this.apiVersion = apiVersion;
-    }
 
     @Bean
     protected ForwardedHeaderFilter forwardedHeaderFilter() {
@@ -45,7 +38,6 @@ public class SwaggerConfig {
     private Info apiInfo() {
         return new Info()
                 .title("momo API")
-                .description("momo API 입니다.")
-                .version(apiVersion);
+                .description("momo API 입니다.");
     }
 }
