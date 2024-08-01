89dfa49ff9e634d6f9dd02f9f2582117d95fed62
hwinkr <dnddl8280@naver.com>
2024-08-02 01:18:13 +0900

    chore: as const 추가, red color 문자열 형태로 수정

--- frontend/src/styles/tokens/colors.ts
+++ frontend/src/styles/tokens/colors.ts
@@ -1,6 +1,9 @@
 const PRIMITIVE_COLORS = {
   black: '#000000',
   white: '#ffffff',
+  // 현재 달력에서 공휴일 색을 표시하기 위해서 빨간색을 사용하고 있는데, 빨간색도 palette를 사용해야 할지는 미정(@해리)
+  red: '#ff0000',
+
   pink: {
     100: '#FFEBE8',
     200: '#FFD4D1',
@@ -23,17 +26,13 @@ const PRIMITIVE_COLORS = {
     800: '#27272a',
     900: '#18181b',
   },
-  // 현재 달력에서 공휴일 색을 표시하기 위해서 빨간색을 사용하고 있는데, 빨간색도 palette를 사용해야 할지는 미정(@해리)
-  red: {
-    100: '#ff0000',
-  },
 } as const;
 
 export const SEMANTIC_COLORS = {
   white: PRIMITIVE_COLORS.white,
   black: PRIMITIVE_COLORS.black,
   primary: PRIMITIVE_COLORS.pink['300'],
-  holiday: PRIMITIVE_COLORS.red['100'],
+  holiday: PRIMITIVE_COLORS.red,
   grey: {
     primary: PRIMITIVE_COLORS.grey['200'],
     dark: PRIMITIVE_COLORS.grey['500'],
@@ -55,4 +54,4 @@ export const SEMANTIC_COLORS = {
       deepDark: PRIMITIVE_COLORS.pink['900'],
     },
   },
-};
+} as const;
