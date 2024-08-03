c4efd54caf08733ccc351f290f1720442adae6dd
ikjo39 <chi4321@naver.com>
2024-08-03 21:40:22 +0900

    refactor(CorsProperties): lombok 활용으로 불필요한 코드 삭제

--- backend/src/main/java/kr/momo/config/CorsProperties.java
+++ backend/src/main/java/kr/momo/config/CorsProperties.java
@@ -1,15 +1,13 @@
 package kr.momo.config;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "security")
+@RequiredArgsConstructor
 @Getter
 public class CorsProperties {
 
     private final String[] allowOrigins;
-
-    public CorsProperties(String[] allowOrigins) {
-        this.allowOrigins = allowOrigins;
-    }
 }
