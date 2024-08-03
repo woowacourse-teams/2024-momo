2a5364d6dc4d0c806b9c1c3882c5ead62725109b
Largopie <106071687+Largopie@users.noreply.github.com>
2024-08-04 01:31:07 +0900

    refactor: line-height속성 삭제

--- frontend/src/styles/tokens/typography.ts
+++ frontend/src/styles/tokens/typography.ts
@@ -14,57 +14,46 @@ const TYPOGRAPHY = {
   titleBold: {
     fontSize: '2.4rem',
     fontWeight: FontWeight['bold'],
-    lineHeight: '1.5',
   },
   titleMedium: {
     fontSize: '2.4rem',
     fontWeight: FontWeight['medium'],
-    lineHeight: '1.5',
   },
   subTitleBold: {
     fontSize: '2.0rem',
     fontWeight: FontWeight['bold'],
-    lineHeight: '1.5',
   },
   subTitleMedium: {
     fontSize: '2.0rem',
     fontWeight: FontWeight['medium'],
-    lineHeight: '1.5',
   },
   subTitleLight: {
     fontSize: '2.0rem',
     fontWeight: FontWeight['light'],
-    lineHeight: '1.5',
   },
   bodyBold: {
     fontSize: '1.6rem',
     fontWeight: FontWeight['bold'],
-    lineHeight: '1.5',
   },
   bodyMedium: {
     fontSize: '1.6rem',
     fontWeight: FontWeight['medium'],
-    lineHeight: '1.5',
   },
   bodyLight: {
     fontSize: '1.6rem',
     fontWeight: FontWeight['light'],
-    lineHeight: '1.5',
   },
   captionBold: {
     fontSize: '1.2rem',
     fontWeight: FontWeight['bold'],
-    lineHeight: '1.3',
   },
   captionMedium: {
     fontSize: '1.2rem',
     fontWeight: FontWeight['medium'],
-    lineHeight: '1.3',
   },
   captionLight: {
     fontSize: '1.2rem',
     fontWeight: FontWeight['light'],
-    lineHeight: '1.3',
   },
 };
 
