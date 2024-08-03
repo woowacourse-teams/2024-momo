992f8c73844435ffef1829a3a25134023ca23bc8
ikjo39 <chi4321@naver.com>
2024-08-03 16:23:21 +0900

    fix(CorsProperties): 불필요한 Validation 어노테이션 제거

--- backend/src/main/java/kr/momo/config/CorsProperties.java
+++ backend/src/main/java/kr/momo/config/CorsProperties.java
@@ -1,17 +1,13 @@
 package kr.momo.config;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.ConstructorBinding;
-import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "security")
 @Getter
-@Validated
 public class CorsProperties {
 
-    @NotNull
     private final String[] allowOrigins;
 
     @ConstructorBinding
