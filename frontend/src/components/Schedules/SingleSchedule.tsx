bc0e90466ac7956cc2c5aa0f06dee54b213af452
hwinkr <dnddl8280@naver.com>
2024-08-04 21:12:30 +0900

    feat: 단일 참여자의 약속 참여 현황을 보여주는 컴포넌트 구현

---
+++ frontend/src/components/Schedules/SingleSchedule.tsx
@@ -0,0 +1,75 @@
+import { css } from '@emotion/react';
+import type { MeetingSingleSchedule } from 'types/meeting';
+
+import { generateScheduleMatrix } from '@components/Time/Picker/TimePicker.util';
+
+import {
+  s_scheduleTable,
+  s_tableHeaderCell,
+  s_td,
+  s_timeColumn,
+  s_timeText,
+} from './Schedules.styles';
+import { formatDate, formatTime } from './Schedules.util';
+
+interface SingleScheduleProps {
+  firstTime: string;
+  lastTime: string;
+  availableDates: string[];
+  singleSchedule: MeetingSingleSchedule;
+}
+
+export default function SingleSchedule({
+  firstTime,
+  lastTime,
+  availableDates,
+  singleSchedule,
+}: SingleScheduleProps) {
+  const schedules = generateScheduleMatrix({
+    firstTime,
+    lastTime,
+    availableDates,
+    meetingSchedules: singleSchedule,
+  });
+
+  return (
+    <div>
+      <table css={s_scheduleTable} aria-label="약속 시간 조회 테이블">
+        <thead>
+          <tr>
+            <th css={s_tableHeaderCell}></th>
+            {availableDates.map((date) => {
+              const { dayOfWeek, monthDate } = formatDate(date);
+              return (
+                <th key={date} css={s_tableHeaderCell}>
+                  <span>{dayOfWeek}</span>
+                  <br />
+                  <span>{monthDate}</span>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {schedules.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              <td css={s_timeColumn}>
+                <span css={s_timeText}>
+                  {formatTime(`${rowIndex + parseInt(firstTime.slice(0, 2))}:00`)}
+                </span>
+              </td>
+              {row.map((_, columnIndex) => (
+                <td
+                  key={columnIndex}
+                  css={css`
+                    ${s_td(schedules[rowIndex][columnIndex])}
+                  `}
+                />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
