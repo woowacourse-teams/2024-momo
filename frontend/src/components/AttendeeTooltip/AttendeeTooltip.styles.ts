e54479f752e07e44a2cea5aa83d562e50a9bdb0e
hwinkr <dnddl8280@naver.com>
2024-08-04 21:10:02 +0900

    feat: 특정 셀 선택한 사용자를 보여주는 툴팁 컴포넌트 구현

---
+++ frontend/src/components/AttendeeTooltip/AttendeeTooltip.styles.ts
@@ -0,0 +1,20 @@
+import { css } from '@emotion/react';
+
+export const s_tooltipTrigger = css`
+  width: 100%;
+  height: 100%;
+`;
+
+export const s_container = css`
+  min-width: max-content;
+`;
+
+export const s_tooltipTitle = css`
+  font-size: 1.6rem;
+  font-weight: 500;
+`;
+
+export const s_attendeeText = css`
+  font-size: 1.2rem;
+  font-weight: 300;
+`;
---
+++ frontend/src/components/AttendeeTooltip/AttendeeTooltip.utils.ts
@@ -0,0 +1,17 @@
+import type { MeetingAllSchedules } from 'types/meeting';
+
+export const findAttendeeNames = (
+  allSchedules: MeetingAllSchedules,
+  availableDates: string[],
+  columnIndex: number,
+  rowIndex: number,
+  firstTime: string,
+): string[] => {
+  const schedule = allSchedules.schedules.find(
+    (s) =>
+      s.date === availableDates[columnIndex] &&
+      s.time === `${rowIndex + parseInt(firstTime.slice(0, 2))}:00`,
+  );
+
+  return schedule ? schedule.attendeeNames : [];
+};
