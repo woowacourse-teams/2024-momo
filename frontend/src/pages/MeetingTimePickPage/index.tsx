f5b3a92d9a3dd96da42ca81f2442ac2434fb5c73
hwinkr <dnddl8280@naver.com>
2024-08-04 21:14:18 +0900

    chore: 약속 참여 페이지에서 사용하는 컴포넌트명 수정
    - time -> schedule

--- frontend/src/pages/MeetingTimePickPage/index.tsx
+++ frontend/src/pages/MeetingTimePickPage/index.tsx
@@ -1,21 +1,14 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { TimePickerUpdateStateContext } from '@contexts/TimePickerUpdateStateProvider';
 
+import SchedulesViewer from '@components/Schedules/SchedulesViewer';
 import TimePickerContainer from '@components/Time/Picker/TimePickerContainer';
-import TimeViewer from '@components/Time/Viewer';
 
 import { useGetMeetingQuery } from '@stores/servers/meeting/queries';
 
-import {
-  s_attendeesContainer,
-  s_container,
-  s_pageHeader,
-  s_tabButton,
-  s_tipInfo,
-  s_title,
-} from './MeetingTimePickPage.styles';
+import { s_container, s_pageHeader, s_tipInfo, s_title } from './MeetingTimePickPage.styles';
 
 const MEETING_QUERY_PAGE_ATTRIBUTES = {
   title: '약속에 참여할 수 있는 시간을\n알려주세요',
@@ -27,15 +20,8 @@ export default function MeetingTimePickPage() {
   const params = useParams<{ uuid: string }>();
   const uuid = params.uuid!;
 
-  const { data: meetingBase } = useGetMeetingQuery(uuid);
-
+  const { data: meetingFrame } = useGetMeetingQuery(uuid);
   const { isTimePickerUpdate } = useContext(TimePickerUpdateStateContext);
-
-  const [selectedAttendee, setSelectedAttendee] = useState('');
-  const handleAttendeeChange = (attendee: string) => {
-    if (selectedAttendee === attendee) return;
-    setSelectedAttendee(attendee);
-  };
 
   return (
     <div css={s_container} aria-label="약속 정보 조회 페이지">
@@ -44,33 +30,19 @@ export default function MeetingTimePickPage() {
         <span css={s_tipInfo}>{MEETING_QUERY_PAGE_ATTRIBUTES.dragInfo}</span>
         <span css={s_tipInfo}>{MEETING_QUERY_PAGE_ATTRIBUTES.timeClickInfo}</span>
       </section>
-      <section css={s_attendeesContainer} aria-label="약속 참가자들 정보">
-        <button css={s_tabButton(true)} onClick={() => handleAttendeeChange('')}>
-          전체
-        </button>
-        {meetingBase?.attendeeNames.map((attendee) => (
-          <button
-            key={attendee}
-            css={s_tabButton(true)}
-            onClick={() => handleAttendeeChange(attendee)}
-          >
-            {attendee}
-          </button>
-        ))}
-      </section>
-      {meetingBase && isTimePickerUpdate ? (
-        <TimePickerContainer
-          firstTime={meetingBase.firstTime}
-          lastTime={meetingBase.lastTime}
-          availableDates={meetingBase.availableDates}
+      {meetingFrame && !isTimePickerUpdate ? (
+        <SchedulesViewer
+          firstTime={meetingFrame?.firstTime}
+          lastTime={meetingFrame?.lastTime}
+          availableDates={meetingFrame?.availableDates}
+          meetingAttendees={meetingFrame?.attendees}
         />
       ) : (
-        meetingBase && (
-          <TimeViewer
-            firstTime={meetingBase.firstTime}
-            lastTime={meetingBase.lastTime}
-            availableDates={meetingBase.availableDates}
-            selectedAttendee={selectedAttendee}
+        meetingFrame && (
+          <TimePickerContainer
+            firstTime={meetingFrame?.firstTime}
+            lastTime={meetingFrame?.lastTime}
+            availableDates={meetingFrame?.availableDates}
           />
         )
       )}
