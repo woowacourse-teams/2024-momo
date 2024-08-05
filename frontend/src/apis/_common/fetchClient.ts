7d08def5b397e3217d3a6b27f317b09d50a4a36d
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-08-05 10:32:44 +0900

    feat(fetchClient): fetchClient에 인증이 필요한 API 요청을 처리할 수 있는 기능 추가 (credentials 옵션 설정)
    isAuthRequire 플래그를 통해 credentials 옵션 설정

--- frontend/src/apis/_common/fetchClient.ts
+++ frontend/src/apis/_common/fetchClient.ts
@@ -7,16 +7,31 @@ interface FetchOption {
   method: HTTPMethod;
   errorMessage?: string;
   body?: object;
-  token?: string;
+  /**
+   * @Yoonkyoungme
+   * isAuthRequire: 인증이 필요한 API 요청인지를 나타내는 플래그
+   * true로 설정하면 요청에 credentials: 'include'가 추가되어,
+   * 서버에서 설정한 인증 쿠키(예: JWT)를 자동으로 포함시킨다.
+   *
+   * @default false
+   */
+  isAuthRequire?: boolean;
 }
 
 // TODO: TypeError: Failed to Fetch에 대한 에러 처리는 어떻게 할 예정인지.
-export async function fetchClient<T>({ url, method, errorMessage, body }: FetchOption) {
+export async function fetchClient<T>({
+  url,
+  method,
+  errorMessage,
+  body,
+  isAuthRequire = false,
+}: FetchOption) {
   const headers = getHeaders();
   const response = await fetch(url, {
     method,
     headers,
     body: body ? JSON.stringify(body) : null,
+    credentials: isAuthRequire ? 'include' : 'omit',
   });
 
   if (!response.ok) {
--- frontend/src/apis/_common/getHeaders.ts
+++ frontend/src/apis/_common/getHeaders.ts
@@ -1,14 +1,3 @@
-import { getCookie } from '@utils/cookies';
-
-import { COOKIE_KEYS } from '@constants/cookies';
-
-export default function getHeaders(): HeadersInit {
-  const headers = { 'Content-type': 'application/json' };
-  const token = getCookie(COOKIE_KEYS.token);
-
-  if (token) {
-    return { ...headers, Authorization: `Bearer ${token}` };
-  }
-
-  return headers;
+export default function getHeaders(): Record<string, string> {
+  return { 'Content-type': 'application/json' };
 }
