fc99d3677689dbd2b9f5037f7014ebd660a2a522
ikjo39 <chi4321@naver.com>
2024-08-05 16:15:07 +0900

    refactor(CorsProperties): `ConfigurationProperties`내 `@Validated` 추가

--- backend/src/main/java/kr/momo/config/CorsProperties.java
+++ backend/src/main/java/kr/momo/config/CorsProperties.java
@@ -1,13 +1,17 @@
 package kr.momo.config;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
+@Validated
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "security")
 public class CorsProperties {
 
+    @NotEmpty
     private final String[] allowOrigins;
 }
