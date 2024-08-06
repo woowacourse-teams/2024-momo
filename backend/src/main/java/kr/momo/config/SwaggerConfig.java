9af7df2c0c85dc9c2c835e4d05d4a9fee6bec45e
Seokmyung Ham <gkatjraud1@gmail.com>
2024-08-07 01:14:46 +0900

    feat(SwaggerConfig): 쿼리 파라미터가 다를경우 별도의 엔드포인트로 인식하도록 Router Operation를 커스터마이징

--- backend/src/main/java/kr/momo/config/SwaggerConfig.java
+++ backend/src/main/java/kr/momo/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package kr.momo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.customizers.RouterOperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
@@ -24,5 +25,16 @@ public class SwaggerConfig {
         return new Info()
                 .title("momo API")
                 .description("momo API 입니다.");
+    }
+
+    @Bean
+    public RouterOperationCustomizer appendQueryParamsToPath() {
+        return (routerOperation, handlerMethod) -> {
+            if (routerOperation.getParams().length > 0) {
+                routerOperation.setPath(
+                        routerOperation.getPath() + "?" + String.join("&", routerOperation.getParams()));
+            }
+            return routerOperation;
+        };
     }
 }
