aba4cd5616c6d4564fa660c83d402739fef4b1af
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-08-06 22:46:14 +0900

    refactor(ScheduleService): createRecommendResponses 메서드 내부 로직에서 인덱스가 아닌 iterator를 사용하도록 수정

--- backend/src/main/java/kr/momo/service/schedule/ScheduleService.java
+++ backend/src/main/java/kr/momo/service/schedule/ScheduleService.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -133,7 +134,7 @@ public class ScheduleService {
         List<ScheduleRecommendResponse> recommendResponses = new ArrayList<>();
         for (AttendeeGroup group : groupCombinations) {
             List<Schedule> schedules = scheduleRepository.findAllByAttendeeIn(group.getAttendees());
-            List<ScheduleRecommendResponse> recommendResponse = groupingScheduleByAttendees(schedules);
+            List<ScheduleRecommendResponse> recommendResponse = createRecommendResponses(schedules);
             List<ScheduleRecommendResponse> response = sortedAndFilterByRecommendType(
                     recommendType, group.size(), recommendResponse
             );
@@ -143,6 +144,45 @@ public class ScheduleService {
         return SchedulesRecommendResponse.of(allAttendeeGroup, recommendResponses);
     }
 
+    private List<ScheduleRecommendResponse> createRecommendResponses(List<Schedule> schedules) {
+        Map<LocalDateTime, AttendeeGroup> attendeeByDateTime = groupAttendeeByDateTime(schedules);
+        Iterator<LocalDateTime> iterator = getIteratorBySortedDateTime(attendeeByDateTime);
+        if (!iterator.hasNext()) {
+            return Collections.emptyList();
+        }
+
+        LocalDateTime startDateTime = iterator.next();
+        AttendeeGroup startGroup = attendeeByDateTime.get(startDateTime);
+
+        LocalDateTime currentDateTime = startDateTime;
+        AttendeeGroup currentGroup = startGroup;
+
+        List<ScheduleRecommendResponse> responses = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            LocalDateTime nextDateTime = iterator.next();
+            AttendeeGroup nextGroup = attendeeByDateTime.get(nextDateTime);
+
+            if (isDiscontinuousDateTime(currentDateTime, nextDateTime, currentGroup, nextGroup)) {
+                responses.add(ScheduleRecommendResponse.of(startDateTime, currentDateTime, startGroup));
+                startDateTime = nextDateTime;
+                startGroup = nextGroup;
+            }
+
+            currentDateTime = nextDateTime;
+            currentGroup = nextGroup;
+        }
+        responses.add(ScheduleRecommendResponse.of(startDateTime, currentDateTime, startGroup));
+        return responses;
+    }
+
+    private Iterator<LocalDateTime> getIteratorBySortedDateTime(Map<LocalDateTime, AttendeeGroup> attendeeByDateTime) {
+        return attendeeByDateTime.keySet().stream()
+                .sorted()
+                .toList()
+                .iterator();
+    }
+
     private List<ScheduleRecommendResponse> sortedAndFilterByRecommendType(
             String recommendType, int groupSize, List<ScheduleRecommendResponse> scheduleRecommendResponses
     ) {
@@ -150,38 +190,6 @@ public class ScheduleService {
                 .filter(response -> response.attendeeNames().size() == groupSize)
                 .sorted(ScheduleRecommender.from(recommendType).getComparator())
                 .toList();
-    }
-
-    private List<ScheduleRecommendResponse> groupingScheduleByAttendees(List<Schedule> schedules) {
-        if (schedules.isEmpty()) {
-            return Collections.emptyList();
-        }
-
-        Map<LocalDateTime, AttendeeGroup> attendeeByDateTime = groupAttendeeByDateTime(schedules);
-        List<LocalDateTime> sortedDateTimes = attendeeByDateTime.keySet().stream()
-                .sorted()
-                .toList();
-
-        LocalDateTime startTime = sortedDateTimes.get(0);
-        AttendeeGroup startNames = attendeeByDateTime.get(startTime);
-
-        LocalDateTime now = startTime;
-        AttendeeGroup nowNames = startNames;
-
-        List<ScheduleRecommendResponse> responses = new ArrayList<>();
-        for (int i = 1; i < sortedDateTimes.size(); i++) {
-            LocalDateTime next = sortedDateTimes.get(i);
-            AttendeeGroup nextNames = attendeeByDateTime.get(next);
-            if (isDiscontinuousDateTime(now, next, nowNames, nextNames)) {
-                responses.add(ScheduleRecommendResponse.of(startTime, now, startNames));
-                startTime = next;
-                startNames = nextNames;
-            }
-            now = next;
-            nowNames = nextNames;
-        }
-        responses.add(ScheduleRecommendResponse.of(startTime, now, startNames));
-        return responses;
     }
 
     private Map<LocalDateTime, AttendeeGroup> groupAttendeeByDateTime(List<Schedule> schedules) {
