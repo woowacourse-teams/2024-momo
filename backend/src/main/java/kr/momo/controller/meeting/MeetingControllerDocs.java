994de7c7a752c34782ddad4d444d01d7d2b41d38
Seokmyung Ham <gkatjraud1@gmail.com>
2024-08-07 20:21:16 +0900

    refactor: 커스텀 어노테이션으로 중복 로직 개선

--- backend/src/main/java/kr/momo/controller/meeting/MeetingControllerDocs.java
+++ backend/src/main/java/kr/momo/controller/meeting/MeetingControllerDocs.java
@@ -1,15 +1,15 @@
 package kr.momo.controller.meeting;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.momo.controller.MomoApiResponse;
+import kr.momo.controller.annotation.ApiErrorResponse;
+import kr.momo.controller.annotation.ApiSuccessResponse;
 import kr.momo.controller.auth.AuthAttendee;
-import kr.momo.exception.CustomProblemDetail;
+import kr.momo.service.meeting.dto.MeetingConfirmRequest;
+import kr.momo.service.meeting.dto.MeetingConfirmResponse;
 import kr.momo.service.meeting.dto.MeetingCreateRequest;
 import kr.momo.service.meeting.dto.MeetingCreateResponse;
 import kr.momo.service.meeting.dto.MeetingResponse;
@@ -21,44 +21,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Tag(name = "Meeting", description = "약속 API")
 public interface MeetingControllerDocs {
 
-    @Operation(
-            summary = "약속 생성",
-            description = "주최자가 약속을 생성하는 API 입니다."
-    )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "201",
-                    description = "약속 생성 성공",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = MeetingCreateResponse.class)
-                    )
-            )
-    })
+    @Operation(summary = "약속 생성", description = "주최자가 약속을 생성하는 API 입니다.")
+    @ApiSuccessResponse.Created(value = "약속 생성 성공")
     ResponseEntity<MomoApiResponse<MeetingCreateResponse>> create(@RequestBody @Valid MeetingCreateRequest request);
 
     @Operation(
-            summary = "약속 정보 조회",
-            description = "약속의 정보를 조회하는 API 입니다."
+            summary = "약속 확정",
+            description = """
+                    약속 일정을 확정하는 API 입니다.<br>
+                    이 요청은 JWT 토큰 인증이 필요합니다.
+                    """
     )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "약속 정보 조회 성공",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = MeetingResponse.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "약속이 존재하지 않습니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            )
-    })
+    @ApiSuccessResponse.Created(value = "약속 확정 성공")
+    @ApiErrorResponse.BadRequest("""
+            요청이 잘못되었습니다.
+            1. 유효하지 않은 UUID
+            2. 유효하지 않은 주최자 정보
+            3. 이미 확정된 약속인 경우
+            4. 약속이 잠겨있지 않은 경우
+            5. 요청 날짜나 시간이 유효하지 않은 경우
+            """
+    )
+    @ApiErrorResponse.Unauthorized("JWT 토큰 인증에 실패하였습니다.")
+    @ApiErrorResponse.Forbidden("주최자 권한만 가능합니다.")
+    ResponseEntity<MomoApiResponse<MeetingConfirmResponse>> confirm(
+            @PathVariable @Schema(description = "약속 UUID") String uuid,
+            @AuthAttendee @Schema(hidden = true) long id,
+            @RequestBody @Valid MeetingConfirmRequest request
+    );
+
+    @Operation(summary = "약속 정보 조회", description = "약속의 정보를 조회하는 API 입니다.")
+    @ApiSuccessResponse.Ok(value = "약속 정보 조회 성공")
+    @ApiErrorResponse.NotFound("약속이 존재하지 않습니다.")
     MomoApiResponse<MeetingResponse> find(@PathVariable @Schema(description = "약속 UUID") String uuid);
 
     @Operation(summary = "약속 공유 정보 조회", deprecated = true)
@@ -71,40 +65,15 @@ public interface MeetingControllerDocs {
                     이 요청은 JWT 토큰 인증이 필요합니다.
                     """
     )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "약속 잠금 성공"
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = """
-                            요청이 잘못되었습니다.
-                            1. 유효하지 않은 UUID
-                            2. 유효하지 않은 주최자 정보
-                            """,
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "401",
-                    description = "JWT 토큰 인증에 실패하였습니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "403",
-                    description = "주최자 권한만 가능합니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            )
-    })
+    @ApiSuccessResponse.Ok("약속 잠금 성공")
+    @ApiErrorResponse.BadRequest("""
+            요청이 잘못되었습니다.
+            1. 유효하지 않은 UUID
+            2. 유효하지 않은 주최자 정보
+            """
+    )
+    @ApiErrorResponse.Unauthorized("JWT 토큰 인증에 실패하였습니다.")
+    @ApiErrorResponse.Forbidden("주최자 권한만 가능합니다.")
     void lock(
             @PathVariable @Schema(description = "약속 UUID") String uuid,
             @AuthAttendee @Schema(hidden = true) long id
@@ -117,40 +86,15 @@ public interface MeetingControllerDocs {
                     이 요청은 JWT 토큰 인증이 필요합니다.
                     """
     )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "약속 잠금 해제 성공"
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = """
-                            요청이 잘못되었습니다.
-                            1. 유효하지 않은 UUID
-                            2. 유효하지 않은 주최자 정보
-                            """,
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "401",
-                    description = "JWT 토큰 인증에 실패하였습니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "403",
-                    description = "주최자 권한만 가능합니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            )
-    })
+    @ApiSuccessResponse.Ok("약속 잠금 해제 성공")
+    @ApiErrorResponse.BadRequest("""
+            요청이 잘못되었습니다.
+            1. 유효하지 않은 UUID
+            2. 유효하지 않은 주최자 정보
+            """
+    )
+    @ApiErrorResponse.Unauthorized("JWT 토큰 인증에 실패하였습니다.")
+    @ApiErrorResponse.Forbidden("주최자 권한만 가능합니다.")
     void unlock(
             @PathVariable @Schema(description = "약속 UUID") String uuid,
             @AuthAttendee @Schema(hidden = true) long id
--- backend/src/main/java/kr/momo/controller/schedule/ScheduleControllerDocs.java
+++ backend/src/main/java/kr/momo/controller/schedule/ScheduleControllerDocs.java
@@ -2,15 +2,13 @@ package kr.momo.controller.schedule;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.momo.controller.MomoApiResponse;
+import kr.momo.controller.annotation.ApiErrorResponse;
+import kr.momo.controller.annotation.ApiSuccessResponse;
 import kr.momo.controller.auth.AuthAttendee;
-import kr.momo.exception.CustomProblemDetail;
 import kr.momo.service.schedule.dto.AttendeeScheduleResponse;
 import kr.momo.service.schedule.dto.ScheduleCreateRequest;
 import kr.momo.service.schedule.dto.SchedulesResponse;
@@ -27,95 +25,36 @@ public interface ScheduleControllerDocs {
                     이 요청은 JWT 토큰 인증이 필요합니다.
                     """
     )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "일정 생성 성공"
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = """
-                            요청이 잘못되었습니다.
-                            1. 유효하지 않은 UUID
-                            2. 약속이 잠겨있는 경우
-                            3. 유효하지 않은 참가자 정보
-                            4. 선택한 날짜가 약속의 후보 날짜가 아닌 경우
-                            5. 선택한 시간이 약속의 시간 범위를 벗어난 경우
-                            """,
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "401",
-                    description = "JWT 토큰 인증에 실패하였습니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            )
-    })
+    @ApiSuccessResponse.Ok("일정 생성 성공")
+    @ApiErrorResponse.BadRequest("""
+            요청이 잘못되었습니다.
+            1. 유효하지 않은 UUID
+            2. 약속이 잠겨있는 경우
+            3. 유효하지 않은 참가자 정보
+            4. 선택한 날짜가 약속의 후보 날짜가 아닌 경우
+            5. 선택한 시간이 약속의 시간 범위를 벗어난 경우
+            """
+    )
+    @ApiErrorResponse.Unauthorized("JWT 토큰 인증에 실패하였습니다.")
     void create(
             @PathVariable @Schema(description = "약속 UUID") String uuid,
             @AuthAttendee @Schema(hidden = true) long id,
             @RequestBody @Valid ScheduleCreateRequest request
     );
 
-    @Operation(
-            summary = "모든 일정 조회",
-            description = """
-                    약속의 모든 일정들을 조회하는 API 입니다.<br>
-                    """
-    )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "조회 성공",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = SchedulesResponse.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "약속이 존재하지 않습니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            )
-    })
+    @Operation(summary = "모든 일정 조회", description = "약속의 모든 일정들을 조회하는 API 입니다.")
+    @ApiSuccessResponse.Ok(value = "조회 성공")
+    @ApiErrorResponse.NotFound("약속이 존재하지 않습니다.")
     MomoApiResponse<SchedulesResponse> findAll(@PathVariable @Schema(description = "약속 UUID") String uuid);
 
-    @Operation(
-            summary = "단일 참가자 일정 조회",
-            description = """
-                    참가자 필터링을 통해 일정을 조회하는 API 입니다.
-                    """
+    @Operation(summary = "단일 참가자 일정 조회", description = "참가자로 일정을 필터링하여 조회하는 API 입니다.")
+    @ApiSuccessResponse.Ok("단일 참가자 일정 조회 성공")
+    @ApiErrorResponse.NotFound("""
+            존재하지 않는 정보입니다.
+            1. 약속 정보가 존재하지 않음
+            2. 약속 참가자 정보가 존재하지 않음
+            """
     )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "단일 참가자 일정 조회 성공",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = AttendeeScheduleResponse.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = """
-                            존재하지 않는 정보입니다.
-                            1. 약속 정보가 존재하지 않음
-                            2. 약속 참가자 정보가 존재하지 않음
-                            """,
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            )
-    })
     MomoApiResponse<AttendeeScheduleResponse> findSchedulesOfAttendee(
             @PathVariable @Schema(description = "약속 UUID") String uuid,
             @Parameter(description = "참가자 이름", example = "모모") String attendeeName
@@ -128,36 +67,14 @@ public interface ScheduleControllerDocs {
                     이 요청은 JWT 토큰 인증이 필요합니다.
                     """
     )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "일정 조회 성공",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = AttendeeScheduleResponse.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = """
-                            존재하지 않는 정보입니다.
-                            1. 약속 정보가 존재하지 않음
-                            2. 약속 참가자 정보가 존재하지 않음
-                            """,
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "401",
-                    description = "JWT 토큰 인증에 실패하였습니다.",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = CustomProblemDetail.class)
-                    )
-            )
-    })
+    @ApiSuccessResponse.Ok("일정 조회 성공")
+    @ApiErrorResponse.Unauthorized("JWT 토큰 인증에 실패하였습니다.")
+    @ApiErrorResponse.NotFound("""
+            존재하지 않는 정보입니다.
+            1. 약속 정보가 존재하지 않음
+            2. 약속 참가자 정보가 존재하지 않음
+            """
+    )
     MomoApiResponse<AttendeeScheduleResponse> findMySchedule(
             @PathVariable @Schema(description = "약속 UUID") String uuid,
             @AuthAttendee @Schema(hidden = true) long id
