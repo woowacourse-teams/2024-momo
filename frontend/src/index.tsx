e22eb0c9ee68db95bcae09bd9b9377021ae46866
hwinkr <dnddl8280@naver.com>
2024-08-07 16:01:40 +0900

    chore: sentry.init 설정 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import { Global, ThemeProvider } from '@emotion/react';
+import * as Sentry from '@sentry/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -16,6 +17,19 @@ const enableMocking = async () => {
 
   return worker.start();
 };
+
+Sentry.init({
+  dsn: `${process.env.SENTRY_DSN}`,
+  environment: process.env.NODE_ENV,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  enabled: process.env.NODE_ENV !== 'development',
+});
 
 const queryClient = new QueryClient();
 
