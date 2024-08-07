e94f238e277bff39af425c3493e4edea03044709
Seokmyung Ham <gkatjraud1@gmail.com>
2024-08-07 20:25:18 +0900

    refactor: 공통 응답 Schema 어노테이션 제거

--- backend/src/main/java/kr/momo/controller/MomoApiResponse.java
+++ backend/src/main/java/kr/momo/controller/MomoApiResponse.java
@@ -1,6 +1,4 @@
 package kr.momo.controller;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
-public record MomoApiResponse<T>(@Schema(description = "응답 데이터") T data) {
+public record MomoApiResponse<T>(T data) {
 }
