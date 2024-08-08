90798c718bc7fa1fcf448465114ba276c6ed926e
Seokmyung Ham <gkatjraud1@gmail.com>
2024-08-09 03:03:29 +0900

    refactor(FilterConfig): 한 가지 구성 정보 책임만 갖도록 FilterConfig 생성

--- backend/src/main/java/kr/momo/config/WebConfig.java
+++ backend/src/main/java/kr/momo/config/WebConfig.java
@@ -1,11 +1,8 @@
 package kr.momo.config;
 
-import jakarta.servlet.Filter;
 import java.util.List;
 import kr.momo.controller.auth.AuthArgumentResolver;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,16 +16,5 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authArgumentResolver);
-    }
-
-    @Bean
-    public FilterRegistrationBean<Filter> logFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-
-        filterRegistrationBean.setFilter(new LogFilter());
-        filterRegistrationBean.setOrder(1);
-        filterRegistrationBean.addUrlPatterns("/api/v1/*");
-
-        return filterRegistrationBean;
     }
 }
