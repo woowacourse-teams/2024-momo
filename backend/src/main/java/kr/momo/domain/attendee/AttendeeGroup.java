e9ce911c0f18bbe8437276a60898df73c22f83c4
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-08-08 17:51:14 +0900

    refactor(AttendeeGroup): equals 코드 오버로딩

--- backend/src/main/java/kr/momo/domain/attendee/AttendeeGroup.java
+++ backend/src/main/java/kr/momo/domain/attendee/AttendeeGroup.java
@@ -6,6 +6,7 @@ import static java.util.stream.Collectors.toList;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.IntStream;
 import kr.momo.exception.MomoException;
@@ -14,7 +15,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
-@EqualsAndHashCode
 public class AttendeeGroup {
 
     private final List<Attendee> attendees;
@@ -78,5 +78,25 @@ public class AttendeeGroup {
 
     public boolean isSameSize(AttendeeGroup attendeeGroup) {
         return attendees.size() == attendeeGroup.size();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        AttendeeGroup that = (AttendeeGroup) object;
+
+        HashSet<Attendee> thatSet = new HashSet<>(that.getAttendees());
+        HashSet<Attendee> set = new HashSet<>(getAttendees());
+        return thatSet.equals(set);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAttendees());
     }
 }
