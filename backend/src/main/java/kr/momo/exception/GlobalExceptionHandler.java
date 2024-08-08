3f841627038021acd8f86f5c4a1c04e29e385249
Seokmyung Ham <gkatjraud1@gmail.com>
2024-08-08 16:20:11 +0900

    refactor(GlobalExceptionHandler): ThreadLocal에 저장되어 있는 TraceId를 로깅에 추가

--- backend/src/main/java/kr/momo/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/kr/momo/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package kr.momo.exception;
 
+import kr.momo.config.LogFilter;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     private static final String INTERNAL_SERVER_ERROR_MESSAGE = "예기치 못한 서버 에러가 발생했습니다.";
+    private static final String EXCEPTION_LOG_FORMAT = "Exception Handler [{}][{}]";
 
     @ExceptionHandler
     public ProblemDetail handleMomoException(MomoException ex) {
-        log.warn(ex.message(), ex);
+        String traceId = MDC.get(LogFilter.TRACE_ID);
+        log.warn(EXCEPTION_LOG_FORMAT, traceId, ex.message(), ex);
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(ex.httpStatus(), ex.message());
         return new CustomProblemDetail(problemDetail, ex.errorCode());
@@ -22,7 +26,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     public ProblemDetail handleInternalException(Exception ex) {
-        log.error(ex.getMessage(), ex);
+        String traceId = MDC.get(LogFilter.TRACE_ID);
+        log.error(EXCEPTION_LOG_FORMAT, traceId, ex.getMessage(), ex);
+
         return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR_MESSAGE);
     }
 }
