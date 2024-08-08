be84ce0c227d68728b57db23085f9ae4d39a0929
hwinkr <dnddl8280@naver.com>
2024-08-08 16:57:06 +0900

    chore: 추천 api 모킹을 위한 recommendHandler 추가

--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -1,5 +1,6 @@
 import { setupWorker } from 'msw/browser';
 
-import meetingHandlers from './meeting/handlers';
+import meetingHandlers from './meeting/meetingHandlers';
+import recommendHandlers from './meeting/recommendHandlers';
 
-export const worker = setupWorker(...meetingHandlers);
+export const worker = setupWorker(...meetingHandlers, ...recommendHandlers);
