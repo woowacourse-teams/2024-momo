e820a8f4b522ddceaa1c5cf3e43c73b41c4d3af7
hwinkr <dnddl8280@naver.com>
2024-08-09 12:12:35 +0900

    feat: 선택된 약속을 확정하기 위한 api 함수 호출 기능 추가

--- frontend/src/pages/MeetingConfirmPage/components/MeetingTimeOptions.tsx
+++ frontend/src/pages/MeetingConfirmPage/components/MeetingTimeOptions.tsx
@@ -1,9 +1,15 @@
+import { useMutation } from '@tanstack/react-query';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import MeetingTimeOptionCard from '@components/MeetingTimeCard/MeetingTimeOptionCard';
+import { Button } from '@components/_common/Buttons/Button';
 import Dropdown from '@components/_common/Dropdown';
 
 import useMeetingTimeRecommendFilter from '@hooks/useMeetingTimeRecommendFilter/useMeetingTimeRecommendFilter';
+
+import { postMeetingConfirm } from '@apis/meetingConfirm';
+import type { MeetingRecommend } from '@apis/meetingRecommend';
 
 import { s_attendeesContainer, s_tabButton, s_tipInfo } from '../MeetingTimeConfirmPage.styles';
 import { s_container } from './MeetingTimeOptions.styles';
@@ -11,6 +17,13 @@ import { s_container } from './MeetingTimeOptions.styles';
 interface MeetingTimeOptionsProps {
   uuid: string;
   attendeeNames: string[];
+}
+
+interface SelectedMeeting {
+  startDate: string;
+  startTime: string;
+  endDate: string;
+  endTime: string;
 }
 
 export default function MeetingTimeOptions({ uuid, attendeeNames }: MeetingTimeOptionsProps) {
@@ -23,7 +36,43 @@ export default function MeetingTimeOptions({ uuid, attendeeNames }: MeetingTimeO
     handleChangeRecommendType,
   } = useMeetingTimeRecommendFilter(uuid, attendeeNames);
 
-  const [selectedMeeting, setSelectedMeeting] = useState(0);
+  const [selectedMeeting, setSelectedMeeting] = useState<SelectedMeeting>({} as SelectedMeeting);
+  const navigate = useNavigate();
+
+  const { mutate } = useMutation({
+    mutationFn: postMeetingConfirm,
+    onSuccess: () => {
+      navigate(`/meeting/${uuid}/fixed-meeting-ticket`);
+    },
+  });
+
+  const handleMeetingConfirmPost = () => {
+    mutate({
+      uuid,
+      requests: selectedMeeting,
+    });
+  };
+
+  // date, time
+  const handleMeetingSelect = (selectedMeeting: MeetingRecommend) => {
+    setSelectedMeeting({
+      startDate: selectedMeeting.startDate,
+      startTime: selectedMeeting.startTime,
+      endDate: selectedMeeting.endDate,
+      endTime: selectedMeeting.endTime,
+    });
+  };
+
+  const checkSelectedMeeting = (meeting: MeetingRecommend) => {
+    return (
+      selectedMeeting.startDate === meeting.startDate &&
+      selectedMeeting.startTime === meeting.startTime &&
+      selectedMeeting.endDate === meeting.endDate &&
+      selectedMeeting.endTime === meeting.endTime
+    );
+  };
+
+  console.log(JSON.stringify(selectedMeeting) === JSON.stringify({}), selectedMeeting, {});
 
   return (
     <div css={s_container}>
@@ -42,6 +91,14 @@ export default function MeetingTimeOptions({ uuid, attendeeNames }: MeetingTimeO
         ))}
       </section>
       <span css={s_tipInfo}>원하는 참여인원을 선택해 보세요 :)</span>
+      <Button
+        disabled={JSON.stringify(selectedMeeting) === JSON.stringify({})}
+        onClick={handleMeetingConfirmPost}
+        size="m"
+        variant="primary"
+      >
+        확정하기
+      </Button>
       <Dropdown
         value={recommendType}
         onChange={(e) => handleChangeRecommendType(e.target.value)}
@@ -54,8 +111,8 @@ export default function MeetingTimeOptions({ uuid, attendeeNames }: MeetingTimeO
         meetingTimeRecommends.map((recommendInfo, index) => (
           <MeetingTimeOptionCard
             key={recommendInfo.startDate + index}
-            isSelected={selectedMeeting === index}
-            onSelect={() => setSelectedMeeting(index)}
+            isSelected={checkSelectedMeeting(recommendInfo)}
+            onSelect={() => handleMeetingSelect(recommendInfo)}
             attendeeCount={attendeeNames.length}
             schedule={recommendInfo}
           />
