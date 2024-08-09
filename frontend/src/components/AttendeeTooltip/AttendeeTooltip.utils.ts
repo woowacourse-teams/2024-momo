443c1b668804778b3c0d15492775440bcd5079fb
Largopie <106071687+Largopie@users.noreply.github.com>
2024-08-09 10:42:22 +0900

    refactor: 참여한 인원 조회 시간 포매팅 함수 수정

--- frontend/src/components/AttendeeTooltip/AttendeeTooltip.utils.ts
+++ frontend/src/components/AttendeeTooltip/AttendeeTooltip.utils.ts
@@ -10,8 +10,11 @@ export const findAttendeeNames = (
   const targetDate = availableDates[columnIndex];
   const targetTime = `${rowIndex + parseInt(firstTime.slice(0, 2))}:00`;
 
+  const formattedTargetTime =
+    parseInt(targetTime.split(':')[0]) < 10 ? `0${targetTime}` : targetTime;
+
   const schedule = allSchedules.schedules.find(
-    (s) => s.date === targetDate && s.time === targetTime,
+    (s) => s.date === targetDate && s.time === formattedTargetTime,
   );
 
   return schedule ? schedule.attendeeNames : [];
