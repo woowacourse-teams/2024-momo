053d21f5d19767bd47a7532d2f6642ff5cf61361
Seokmyung Ham <gkatjraud1@gmail.com>
2024-08-14 14:20:01 +0900

    refactor(ScheduleRepository): Schedule 조회 시 AvailableDate 정보도 함께 조회할 수 있도록 @EntityGraph 적용

--- backend/src/main/java/kr/momo/domain/schedule/ScheduleRepository.java
+++ backend/src/main/java/kr/momo/domain/schedule/ScheduleRepository.java
@@ -2,12 +2,15 @@ package kr.momo.domain.schedule;
 
 import java.util.List;
 import kr.momo.domain.attendee.Attendee;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
+    @EntityGraph(attributePaths = {"availableDate"})
     List<Schedule> findAllByAttendee(Attendee attendee);
 
+    @EntityGraph(attributePaths = {"availableDate"})
     List<Schedule> findAllByAttendeeIn(List<Attendee> attendees);
 
     void deleteAllByAttendee(Attendee attendee);
