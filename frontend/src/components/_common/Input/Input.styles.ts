a4b6a925ac0f8d4fe8799bab3441b24c42665c31
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-08-14 16:08:36 +0900

    design(Button): variant 속성 추가 (default, transparent)

--- frontend/src/components/_common/Input/Input.styles.ts
+++ frontend/src/components/_common/Input/Input.styles.ts
@@ -1,7 +1,22 @@
 import { css } from '@emotion/react';
 
-export const s_input = css`
+import theme from '@styles/theme';
+
+const baseInputStyle = css`
   width: 100%;
   height: 4.4rem;
   padding: 0.8rem;
+  outline-color: ${theme.colors.primary};
 `;
+
+export const s_input = {
+  default: css`
+    ${baseInputStyle}
+    border: 1px solid ${theme.colors.grey.dark};
+  `,
+  transparent: css`
+    ${baseInputStyle}
+    border: none;
+    outline: none;
+  `,
+};
