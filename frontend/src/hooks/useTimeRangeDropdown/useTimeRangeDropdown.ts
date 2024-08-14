79c0c5df9d695a17aaf924737cbd921d68290a71
Largopie <106071687+Largopie@users.noreply.github.com>
2024-08-14 21:59:33 +0900

    refactor(useTimeRangeDropdown): 사용성 개선을 위한 로직 수정
    - startTime을 설정하면 endTime은 startTime + 1시간으로 설정되도록 수정
    - options 목록을 해당 hook에서 내려줄 수 있도록 수정

--- frontend/src/hooks/useTimeRangeDropdown/useTimeRangeDropdown.ts
+++ frontend/src/hooks/useTimeRangeDropdown/useTimeRangeDropdown.ts
@@ -1,20 +1,26 @@
 import { useState } from 'react';
 
 import { INITIAL_END_TIME, INITIAL_START_TIME } from './constants';
-import { isTimeSelectable } from './useTimeRangeDropdown.utils';
+import {
+  generateEndTimeOptions,
+  generateStartTimeOptions,
+  getTimeAfterOneHour,
+  isTimeSelectable,
+} from './useTimeRangeDropdown.utils';
 
 export default function useTimeRangeDropdown() {
   const [startTime, setStartTime] = useState(INITIAL_START_TIME);
   const [endTime, setEndTime] = useState(INITIAL_END_TIME);
 
-  // 시작 시간이 끝 시간보다 빠르다면 startTime이 변경되지 않도록 설정
+  // startTime의 onChange 핸들러 함수
+  // startTime을 선택하면 endTime은 (startTime + 1시간)으로 설정됩니다.
   const handleStartTimeChange = (time: string) => {
-    if (!isTimeSelectable(time, endTime)) return;
-
     setStartTime(time);
+    setEndTime(getTimeAfterOneHour(time));
   };
 
-  // 시작 시간이 끝 시간보다 빠르다면 startTime이 변경되지 않도록 설정
+  // endTime의 onChange 핸들러 함수
+  // startTime이 endTime보다 빠르다면 startTime이 변경되지 않습니다.
   const handleEndTimeChange = (time: string) => {
     if (!isTimeSelectable(startTime, time)) return;
 
@@ -22,9 +28,15 @@ export default function useTimeRangeDropdown() {
   };
 
   return {
-    startTime,
-    endTime,
-    onStartTimeChange: handleStartTimeChange,
-    onEndTimeChange: handleEndTimeChange,
+    startTime: {
+      value: startTime,
+      options: generateStartTimeOptions(),
+    },
+    endTime: {
+      value: endTime,
+      options: generateEndTimeOptions(startTime),
+    },
+    handleStartTimeChange,
+    handleEndTimeChange,
   } as const;
 }
