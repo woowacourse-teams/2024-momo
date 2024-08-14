5d4657083823ea38b439d5aff7fac0f623535317
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-08-15 00:16:24 +0900

    design(PasswordInput): s_inputContainer 흰색 배경 설정

--- frontend/src/components/PasswordInput/PasswordInput.styles.ts
+++ frontend/src/components/PasswordInput/PasswordInput.styles.ts
@@ -6,6 +6,8 @@ export const s_inputContainer = css`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  background-color: ${theme.colors.white};
   border: 1px solid ${theme.colors.grey.dark};
 
   :focus-within {
