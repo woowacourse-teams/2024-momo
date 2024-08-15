1b019eac4b3164cbee17dffcbdf3a6dad04f9ea7
hwinkr <dnddl8280@naver.com>
2024-08-15 17:27:53 +0900

    design: 캘린더 스타일 파일 수정
    - 오늘 표시하는 UI 변경
    - 특정 날짜를 강조하는 스타일 함수에 적용한 early return 패턴에 {} 제거

--- frontend/src/components/_common/Calendar/Calendar.styles.ts
+++ frontend/src/components/_common/Calendar/Calendar.styles.ts
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import type { FlagObject } from 'types/utility';
 
 import theme from '@styles/theme';
 
@@ -35,17 +36,15 @@ export const s_baseDayOfWeek = css`
 `;
 
 export const s_dayOfWeek = (index: number) => {
-  if (index === CALENDAR_PROPERTIES.sundayNumber) {
+  if (index === CALENDAR_PROPERTIES.sundayNumber)
     return css`
       ${DAY_SLOT_TEXT_STYLES.holiday}
     `;
-  }
 
-  if (index === CALENDAR_PROPERTIES.saturdayNumber) {
+  if (index === CALENDAR_PROPERTIES.saturdayNumber)
     return css`
       ${DAY_SLOT_TEXT_STYLES.saturday}
     `;
-  }
 
   return css`
     ${DAY_SLOT_TEXT_STYLES.default}
@@ -84,9 +83,6 @@ export const s_baseDaySlot = css`
   width: 100%;
   min-width: 3.6rem;
   height: 3.6rem;
-
-  /* 모바일에서 계란처럼 보이는 이유가 버튼에 기본 padding이 있는 문제라서, 0으로 임시 해결하긴 했습니다. 글로벌 스타일 버튼을 어떻게 만들어가야할지는 논의가 필요할 것 같아요(@해리) */
-  padding: 0;
 `;
 
 export const s_daySlotButton = css`
@@ -101,6 +97,8 @@ export const s_daySlotButton = css`
 `;
 
 export const s_baseDaySlotText = css`
+  position: relative;
+
   display: flex;
   align-items: center;
   justify-content: center;
@@ -109,50 +107,27 @@ export const s_baseDaySlotText = css`
   height: 3.6rem;
 `;
 
-export const s_daySlotText = (
-  isHoliday: boolean,
-  isSelected: boolean,
-  isToday: boolean,
-  isSaturday: boolean,
-  isPrevDay: boolean,
-) => {
+type DaySlotStatus = 'isSelectedFullDate' | 'isPrevDate' | 'isHoliday' | 'isSaturday' | 'isToday';
+
+export const s_daySlotText = ({
+  isSelectedFullDate,
+  isPrevDate,
+  isHoliday,
+  isSaturday,
+  isToday,
+}: FlagObject<DaySlotStatus>) => {
   /* 덕지덕지 if문인데 어쩔 수 없다고 생각하기는 했습니다. 가독성을 위해서 switch문을 사용하는 것도 고려해보면 좋을 것 같은데, 코멘트로 의견 부탁드려요(@해리) */
   /* if문 위에서부터 아래로, 스타일이 적용되어야 하는 우선순위입니다. 선택된 날짜의 스타일이 가장 우선적으로 고려되어야 하고, 그 다음은 지난날짜,,,순 입니다. 그래서 early return 패턴을 활용했어요(@해리) */
-  if (isSelected) {
-    return css`
-      ${DAY_SLOT_TEXT_STYLES.selected}
-    `;
-  }
+  if (isSelectedFullDate) return DAY_SLOT_TEXT_STYLES.selected;
+  if (isPrevDate) return DAY_SLOT_TEXT_STYLES.prevDay;
+  if (isHoliday) return DAY_SLOT_TEXT_STYLES.holiday;
+  if (isSaturday) return DAY_SLOT_TEXT_STYLES.saturday;
+  if (isToday) return DAY_SLOT_TEXT_STYLES.today;
 
-  if (isPrevDay) {
-    return css`
-      ${DAY_SLOT_TEXT_STYLES.prevDay}
-    `;
-  }
-
-  if (isHoliday) {
-    return css`
-      ${DAY_SLOT_TEXT_STYLES.holiday}
-    `;
-  }
-
-  if (isToday) {
-    return css`
-      ${DAY_SLOT_TEXT_STYLES.today}
-    `;
-  }
-
-  if (isSaturday) {
-    return css`
-      ${DAY_SLOT_TEXT_STYLES.saturday}
-    `;
-  }
-
-  return css`
-    ${DAY_SLOT_TEXT_STYLES.default}
-  `;
+  return DAY_SLOT_TEXT_STYLES.default;
 };
 
+// background-color: ${theme.colors.calendar.backgroundColor.today};
 const DAY_SLOT_TEXT_STYLES = {
   selected: css`
     color: ${theme.colors.calendar.color.selected};
@@ -164,8 +139,22 @@ const DAY_SLOT_TEXT_STYLES = {
   `,
   today: css`
     color: ${theme.colors.calendar.color.today};
-    background-color: ${theme.colors.calendar.backgroundColor.today};
     border-radius: 50%;
+
+    &::after {
+      content: '';
+
+      position: absolute;
+      bottom: 0.4rem;
+      left: 50%;
+      transform: translateX(-50%);
+
+      width: 0.4rem;
+      height: 0.4rem;
+
+      background-color: ${theme.colors.calendar.color.today};
+      border-radius: 50%;
+    }
   `,
   saturday: css`
     color: #8c9eff;
