97ffa87165b2835a6077922f7f688c8991362e53
Largopie <106071687+Largopie@users.noreply.github.com>
2024-08-16 15:53:17 +0900

    refactor: 링크 복사 UI 개선
    - 링크 주소를 미리볼 수 있도록 수정
    - 복사 성공 시, alert 대신 체크 애니메이션 보이도록 수정

--- frontend/src/pages/MeetingLinkSharePage/MeetingLinkSharePage.styles.ts
+++ frontend/src/pages/MeetingLinkSharePage/MeetingLinkSharePage.styles.ts
@@ -1,4 +1,6 @@
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
+
+import theme from '@styles/theme';
 
 export const s_container = css`
   display: flex;
@@ -35,4 +37,71 @@ export const s_buttonContainer = css`
 
 export const s_button = css`
   height: 2.8rem;
+`;
+
+export const s_copyContainer = css`
+  display: flex;
+  align-items: center;
+
+  width: 100%;
+  height: 3.6rem;
+
+  background-color: ${theme.colors.white};
+  border-radius: 0.8rem;
+`;
+
+export const s_urlText = css`
+  overflow: hidden;
+  display: block;
+
+  width: calc(100% - 3.6rem);
+  height: 100%;
+  padding: 0.8rem 0 0.8rem 0.8rem;
+
+  color: ${theme.colors.grey.dark};
+  text-align: center;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+
+  border-radius: 0.8rem;
+`;
+
+export const s_copyButtonContainer = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 3.6rem;
+  height: 3.6rem;
+`;
+
+export const s_copyButton = css`
+  width: 100%;
+  height: 100%;
+  background-color: inherit;
+  border-radius: 0.8rem;
+
+  &:hover {
+    opacity: 0.3;
+  }
+`;
+
+const drawCheck = keyframes`
+  to {
+    stroke-dashoffset: 0;
+  }
+`;
+
+export const s_check = css`
+  path {
+    fill: none;
+    stroke: ${theme.colors.green.deepDark};
+    stroke-dasharray: 100;
+    stroke-dashoffset: 100;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke-width: 8;
+
+    animation: ${drawCheck} 0.8s ease-in-out forwards;
+  }
 `;
--- frontend/src/pages/MeetingLinkSharePage/index.tsx
+++ frontend/src/pages/MeetingLinkSharePage/index.tsx
@@ -1,19 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { Button } from '@components/_common/Buttons/Button';
 
 import { copyToClipboard } from '@utils/clipboard';
 
+import CheckIcon from '@assets/images/check.svg';
+import CopyIcon from '@assets/images/copy.svg';
 import KakaoIcon from '@assets/images/kakao.svg';
 import LogoSunglass from '@assets/images/logoSunglass.svg';
 
 import {
   s_button,
   s_buttonContainer,
+  s_check,
   s_container,
+  s_copyButton,
+  s_copyButtonContainer,
+  s_copyContainer,
   s_description,
   s_meetingInfo,
+  s_urlText,
 } from './MeetingLinkSharePage.styles';
 
 declare global {
@@ -24,11 +31,12 @@ declare global {
 }
 
 export default function MeetingLinkSharePage() {
+  const [copyComplete, setCopyComplete] = useState(false);
   const { Kakao } = window;
   const navigate = useNavigate();
   const params = useParams<{ uuid: string }>();
   const uuid = params.uuid!;
-  const LINK = `${window.location.host}/meeting/${uuid}`;
+  const LINK = `${window.location.protocol}//${window.location.host}/meeting/${uuid}`;
 
   // TODO: 약속명이 있다면, ${userName}을 templateArgs에 같이 삽입(@낙타)
   const handleShareButton = () => {
@@ -40,6 +48,12 @@ export default function MeetingLinkSharePage() {
     });
   };
 
+  const handleCopyClick = () => {
+    copyToClipboard(LINK);
+    setCopyComplete(true);
+    setTimeout(() => setCopyComplete(false), 2500);
+  };
+
   useEffect(() => {
     if (!Kakao.isInitialized()) {
       Kakao.init(process.env.KAKAO_KEY);
@@ -48,14 +62,23 @@ export default function MeetingLinkSharePage() {
 
   return (
     <div css={s_container}>
-      <LogoSunglass width="220" height="220" />
+      <LogoSunglass width="160" height="160" />
       <div css={s_meetingInfo}>
         {/* TODO: '${약속 명} 약속을 만들었어요 :)'로 변경 논의 후 적용 (@Yoonkyoungme) */}
         <div css={s_description}>약속을 만들었어요 :)</div>
         <div css={s_buttonContainer}>
-          <Button size="full" variant="secondary" onClick={() => copyToClipboard(LINK)}>
-            링크 복사
-          </Button>
+          <div css={s_copyContainer}>
+            <span css={s_urlText}>{LINK}</span>
+            <div css={s_copyButtonContainer}>
+              {copyComplete ? (
+                <CheckIcon css={s_check} width="24" height="24" />
+              ) : (
+                <button css={s_copyButton} onClick={handleCopyClick}>
+                  <CopyIcon width="24" height="24" />
+                </button>
+              )}
+            </div>
+          </div>
           <Button
             size="full"
             variant="primary"
