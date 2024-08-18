474913dd0425edb3590ddd61cdd98d9fe09072d1
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-08-19 04:07:14 +0900

    fix(PasswordInput): PasswordInput 컴포넌트에 border가 없어진 문제 수정

--- frontend/src/components/PasswordInput/PasswordInput.styles.ts
+++ frontend/src/components/PasswordInput/PasswordInput.styles.ts
@@ -6,9 +6,13 @@ export const s_inputContainer = css`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
   background-color: ${theme.colors.white};
+  border: 1px solid ${theme.colors.grey.dark};
+  border-radius: 0.3rem;
 
   :focus-within {
+    border: 1px solid ${theme.colors.primary};
     outline: 1px solid ${theme.colors.primary};
   }
 `;
