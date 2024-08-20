3a05b7d9bedb7e44d428c551b321adc38ab1f9ac
Seokmyung Ham <gkatjraud1@gmail.com>
2024-08-20 22:27:54 +0900

    refactor(ScheduleBatchRepository): 배치 업데이트 로직 개선

--- backend/src/main/java/kr/momo/domain/schedule/ScheduleBatchRepository.java
+++ backend/src/main/java/kr/momo/domain/schedule/ScheduleBatchRepository.java
@@ -1,14 +1,12 @@
 package kr.momo.domain.schedule;
 
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.List;
-import lombok.NonNull;
+import java.util.Collection;
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ParameterizedPreparedStatementSetter;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,31 +14,34 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ScheduleBatchRepository {
 
+    private static final int BATCH_SIZE = 1000;
+
     private final JdbcTemplate jdbcTemplate;
 
     @Transactional
-    public void batchInsert(List<Schedule> schedules) {
+    public void batchInsert(Collection<Schedule> schedules) {
         String sql = """
-                    INSERT INTO schedule (attendee_id, available_date_id, timeslot, created_at, modified_at)
-                    VALUES (?, ?, ?, ?, ?);
+                INSERT INTO schedule (attendee_id, available_date_id, timeslot, created_at, modified_at)
+                VALUES (?, ?, ?, ?, ?);
                 """;
 
-        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(@NonNull PreparedStatement ps, int i) throws SQLException {
-                Schedule schedule = schedules.get(i);
-                LocalDateTime now = LocalDateTime.now();
-                ps.setLong(1, schedule.attendeeId());
-                ps.setLong(2, schedule.availableDateId());
-                ps.setString(3, schedule.getTimeslot().toString());
-                ps.setTimestamp(4, Timestamp.valueOf(now));
-                ps.setTimestamp(5, Timestamp.valueOf(now));
-            }
+        executeBatchUpdate(schedules, sql);
+    }
 
-            @Override
-            public int getBatchSize() {
-                return schedules.size();
-            }
-        });
+    private void executeBatchUpdate(Collection<Schedule> schedules, String sql) {
+        LocalDateTime now = LocalDateTime.now();
+        Timestamp timestamp = Timestamp.valueOf(now);
+
+        jdbcTemplate.batchUpdate(sql, schedules, BATCH_SIZE, createPreparedStatementSetter(timestamp));
+    }
+
+    private ParameterizedPreparedStatementSetter<Schedule> createPreparedStatementSetter(Timestamp timestamp) {
+        return (PreparedStatement ps, Schedule schedule) -> {
+            ps.setLong(1, schedule.attendeeId());
+            ps.setLong(2, schedule.availableDateId());
+            ps.setString(3, schedule.getTimeslot().toString());
+            ps.setTimestamp(4, timestamp);
+            ps.setTimestamp(5, timestamp);
+        };
     }
 }
