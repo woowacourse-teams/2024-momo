2f323f478d1e9f472345dfffb5f5b168c38d5b20
hwinkr <dnddl8280@naver.com>
2024-08-20 15:31:49 +0900

    chore: variant에서 accent 타입 제거

--- frontend/src/components/_common/Text/Text.types.ts
+++ frontend/src/components/_common/Text/Text.types.ts
@@ -1,4 +1,4 @@
 import type TYPOGRAPHY from '@styles/tokens/typography';
 
-export type TextVariant = 'default' | 'caption' | 'warning' | 'accent';
+export type TextVariant = 'default' | 'caption' | 'warning';
 export type TextTypo = keyof typeof TYPOGRAPHY;
