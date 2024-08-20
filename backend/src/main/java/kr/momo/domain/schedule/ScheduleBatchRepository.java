dda9e031dab8956f74cf475372545676d9fd9c64
Seokmyung Ham <gkatjraud1@gmail.com>
2024-08-20 20:53:50 +0900

    fix: 잘못된 batch size 수정

--- backend/src/main/java/kr/momo/domain/schedule/ScheduleBatchRepository.java
+++ backend/src/main/java/kr/momo/domain/schedule/ScheduleBatchRepository.java
@@ -16,8 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ScheduleBatchRepository {
 
-    private static final int BATCH_SIZE = 1000;
-
     private final JdbcTemplate jdbcTemplate;
 
     @Transactional
@@ -41,7 +39,7 @@ public class ScheduleBatchRepository {
 
             @Override
             public int getBatchSize() {
-                return BATCH_SIZE;
+                return schedules.size();
             }
         });
     }
