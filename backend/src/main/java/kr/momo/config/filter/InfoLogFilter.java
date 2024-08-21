5dd58c187aeb31705195b36c7f21df25b763bc02
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-08-21 13:51:12 +0900

    feat: 포트번호, 연결 데이터베이스, 활성화된 프로파일도 info 로그로 나타내도록 수정

--- backend/src/main/java/kr/momo/config/filter/InfoLogFilter.java
+++ backend/src/main/java/kr/momo/config/filter/InfoLogFilter.java
@@ -5,11 +5,21 @@ import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 
 public class InfoLogFilter extends Filter<ILoggingEvent> {
+
     @Override
     public FilterReply decide(ILoggingEvent iLoggingEvent) {
-        if (iLoggingEvent.getMessage().contains("REQUEST") || iLoggingEvent.getMessage().contains("RESPONSE")) {
+        if (isNotFilteredLog(iLoggingEvent)) {
             return FilterReply.ACCEPT;
         }
         return FilterReply.DENY;
+    }
+
+    private boolean isNotFilteredLog(ILoggingEvent iLoggingEvent) {
+        StackTraceElement[] callerData = iLoggingEvent.getCallerData();
+        String className = callerData[0].getClassName();
+        return className.contains("LogGenerator")
+                || className.contains("SpringApplication")
+                || className.contains("TomcatWebServer")
+                || className.contains("HikariPool");
     }
 }
