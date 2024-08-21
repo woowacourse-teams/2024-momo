f611ca6f2545cd2f47cd02769cb28849b2674784
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-08-21 23:35:47 +0900

    design(ActionButtonGroup): ActionButtonGroup 컴포넌트 스타일 적용

--- frontend/src/pages/FixedMeetingTicketPage/components/ActionButtonGroup/index.tsx
+++ frontend/src/pages/FixedMeetingTicketPage/components/ActionButtonGroup/index.tsx
@@ -5,6 +5,13 @@ import { useCancelFixedMeetingMutation } from '@stores/servers/confirm/mutations
 import CancelIcon from '@assets/images/cancel.svg';
 import KakaoIcon from '@assets/images/kakao.svg';
 
+import {
+  s_buttonText,
+  s_cancelCircleButton,
+  s_container,
+  s_kakaoCircleButton,
+} from './ActionButtonGroup.styles';
+
 function ActionButtonGroup() {
   const params = useParams<{ uuid: string }>();
   const uuid = params.uuid!;
@@ -20,14 +27,14 @@ function ActionButtonGroup() {
   };
 
   return (
-    <div>
-      <button onClick={() => handleCancelFixedMeetingButton()}>
+    <div css={s_container}>
+      <button css={s_kakaoCircleButton} onClick={() => handleCancelFixedMeetingButton()}>
         <KakaoIcon width="48" height="48" />
       </button>
 
-      <button onClick={() => handleCancelFixedMeetingButton()}>
+      <button css={s_cancelCircleButton} onClick={() => handleCancelFixedMeetingButton()}>
         <CancelIcon width="16" height="16" />
-        <div>확정 취소</div>
+        <div css={s_buttonText}>확정 취소</div>
       </button>
     </div>
   );
