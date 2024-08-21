df8e06acc57d1d2d5ad8bbf74ccb4c2fccdd28ba
Largopie <106071687+Largopie@users.noreply.github.com>
2024-08-22 01:05:21 +0900

    design(NotFoundPage): page not found(404)에러를 나타내는 페이지 UI 구성

---
+++ frontend/src/pages/NotFoundPage/index.tsx
@@ -0,0 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '@components/_common/Buttons/Button';
+import Text from '@components/_common/Text';
+
+import QuestionMomoCharacter from '@assets/images/questionMomoCharacter.svg';
+
+import { CREATE_MEETING_PATH } from '@constants/routes/meeting';
+
+import { s_container, s_textContainer } from './NotFound.styles';
+
+export default function NotFoundPage() {
+  const navigate = useNavigate();
+
+  return (
+    <div css={s_container}>
+      <QuestionMomoCharacter width="128" height="180" />
+      <div css={s_textContainer}>
+        <Text typo="titleBold">
+          <Text.Accent>404</Text.Accent>
+        </Text>
+        <Text typo="bodyBold">원하시는 페이지를 찾을 수 없어요 :(</Text>
+      </div>
+
+      <Button onClick={() => navigate(CREATE_MEETING_PATH)} variant="primary" size="full">
+        약속 생성하러가기
+      </Button>
+    </div>
+  );
+}
