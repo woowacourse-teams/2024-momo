b56d94a0a94c4797b9433a25bb0b1397d2e00d71
HyungUk Ryu <hw0603@naver.com>
2024-08-23 04:51:34 +0900

    refactor(CandidateScheduleSorter): `@FunctionalInterface` 어노테이션 명시

--- backend/src/main/java/kr/momo/domain/schedule/recommend/CandidateScheduleSorter.java
+++ backend/src/main/java/kr/momo/domain/schedule/recommend/CandidateScheduleSorter.java
@@ -2,6 +2,7 @@ package kr.momo.domain.schedule.recommend;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface CandidateScheduleSorter {
 
     void sort(List<CandidateSchedule> unorderedSchedules);
