1f91a2c2a4a0884d5600132377ff1d1dc5e012ef
hwinkr <dnddl8280@naver.com>
2024-08-23 03:43:31 +0900

    design: disabled 스타일 속성 추가

--- frontend/src/components/Schedules/Schedules.styles.ts
+++ frontend/src/components/Schedules/Schedules.styles.ts
@@ -198,4 +198,8 @@ export const s_circleButton = css`
   border: 1px solid ${theme.colors.primary};
   border-radius: 50%;
   box-shadow: 0 4px 4px rgb(0 0 0 / 25%);
+
+  &:disabled {
+    opacity: 0.3;
+  }
 `;
