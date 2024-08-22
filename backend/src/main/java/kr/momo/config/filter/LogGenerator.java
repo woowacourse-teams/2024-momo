4942a3ba69aa7821f89767e485975e3acc1e501b
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-08-22 15:19:49 +0900

    feat: 요청에 사용자 정보 추가

--- backend/src/main/java/kr/momo/config/filter/LogGenerator.java
+++ backend/src/main/java/kr/momo/config/filter/LogGenerator.java
@@ -1,19 +1,36 @@
 package kr.momo.config.filter;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Optional;
+import kr.momo.service.auth.JwtManager;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class LogGenerator {
+
+    private final JwtManager jwtManager;
+
 
     public void logRequest(String traceId, HttpServletRequest request) {
         String httpMethod = request.getMethod();
         String requestURI = request.getRequestURI();
+        Cookie[] cookies = request.getCookies();
+        String token = getCookieValue(cookies).orElse("all");
 
-        log.info("REQUEST [{}][{} {}]", traceId, httpMethod, requestURI);
+        if (token.equals("all")) {
+            log.info("REQUEST [{}][{} {}][{}]", traceId, httpMethod, requestURI, token);
+            return;
+        }
+        long userId = jwtManager.extract(token);
+
+        log.info("REQUEST [{}] [USERID:{}] [{} {}]", traceId, userId, httpMethod, requestURI);
     }
 
     public void logResponse(String traceId, long duration, HttpServletRequest request, HttpServletResponse response) {
@@ -22,5 +39,16 @@ public class LogGenerator {
         int status = response.getStatus();
 
         log.info("RESPONSE [{}][{} {}][{} ms][Status: {}]", traceId, httpMethod, requestURI, duration, status);
+    }
+
+    private Optional<String> getCookieValue(Cookie[] cookies) {
+        if (cookies == null) {
+            return Optional.empty();
+        }
+
+        return Arrays.stream(cookies)
+                .filter(cookie -> "ACCESS_TOKEN".equals(cookie.getName()))
+                .map(Cookie::getValue)
+                .findFirst();
     }
 }
