526a48dff7c2e4cc6c02ade16a080cfe736be7f4
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-08-23 01:38:38 +0900

    rename: svg 파일명 변경 (SadMomo → SadMomoCharacter)

--- frontend/src/pages/FixedMeetingTicketPage/components/UnconfirmedMessage/index.tsx
+++ frontend/src/pages/FixedMeetingTicketPage/components/UnconfirmedMessage/index.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { Button } from '@components/_common/Buttons/Button';
 
-import SadMomo from '@assets/images/sadMomo.svg';
+import SadMomoCharacter from '@assets/images/SadMomoCharacter.svg';
 
 import { s_container } from './UnconfirmedMessage.styles';
 
@@ -18,7 +18,7 @@ function UnconfirmedMessage({ uuid }: UnconfirmedMessageProps) {
 
   return (
     <div css={s_container}>
-      <SadMomo width="128" height="128" />
+      <SadMomoCharacter width="128" height="128" />
       <div>약속이 아직 확정되지 않았어요 :(</div>
       <Button variant="primary" size="full" onClick={() => handleMeetingPageNavigate()}>
         약속 조회하러 가기
