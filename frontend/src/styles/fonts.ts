fc8735df3da38679ade7639281e6b7ead7fee699
Largopie <106071687+Largopie@users.noreply.github.com>
2024-08-23 05:23:15 +0900

    feat: Pretendard 폰트 추가 및 적용

--- frontend/src/styles/fonts.ts
+++ frontend/src/styles/fonts.ts
@@ -2,32 +2,56 @@ import { css } from '@emotion/react';
 
 export const fontFaces = css`
   @font-face {
-    font-family: 'Spoqa Han Sans Neo';
+    font-family: Pretendard;
     font-weight: 100;
-    src: url(${require('../assets/fonts/SpoqaHanSansNeo-Thin.woff2')}) format('woff2');
+    src: url(${require('../assets/fonts/Pretendard-Thin.woff2')}) format('woff2');
   }
 
   @font-face {
-    font-family: 'Spoqa Han Sans Neo';
+    font-family: Pretendard;
+    font-weight: 200;
+    src: url(${require('../assets/fonts/Pretendard-ExtraLight.woff2')}) format('woff2');
+  }
+
+  @font-face {
+    font-family: Pretendard;
     font-weight: 300;
-    src: url(${require('../assets/fonts/SpoqaHanSansNeo-Light.woff2')}) format('woff2');
+    src: url(${require('../assets/fonts/Pretendard-Light.woff2')}) format('woff2');
   }
 
   @font-face {
-    font-family: 'Spoqa Han Sans Neo';
+    font-family: Pretendard;
     font-weight: 400;
-    src: url(${require('../assets/fonts/SpoqaHanSansNeo-Regular.woff2')}) format('woff2');
+    src: url(${require('../assets/fonts/Pretendard-Regular.woff2')}) format('woff2');
   }
 
   @font-face {
-    font-family: 'Spoqa Han Sans Neo';
+    font-family: Pretendard;
     font-weight: 500;
-    src: url(${require('../assets/fonts/SpoqaHanSansNeo-Medium.woff2')}) format('woff2');
+    src: url(${require('../assets/fonts/Pretendard-Medium.woff2')}) format('woff2');
   }
 
   @font-face {
-    font-family: 'Spoqa Han Sans Neo';
+    font-family: Pretendard;
+    font-weight: 600;
+    src: url(${require('../assets/fonts/Pretendard-SemiBold.woff2')}) format('woff2');
+  }
+
+  @font-face {
+    font-family: Pretendard;
     font-weight: 700;
-    src: url(${require('../assets/fonts/SpoqaHanSansNeo-Bold.woff2')}) format('woff2');
+    src: url(${require('../assets/fonts/Pretendard-Bold.woff2')}) format('woff2');
+  }
+
+  @font-face {
+    font-family: Pretendard;
+    font-weight: 800;
+    src: url(${require('../assets/fonts/Pretendard-ExtraBold.woff2')}) format('woff2');
+  }
+
+  @font-face {
+    font-family: Pretendard;
+    font-weight: 900;
+    src: url(${require('../assets/fonts/Pretendard-Black.woff2')}) format('woff2');
   }
 `;
