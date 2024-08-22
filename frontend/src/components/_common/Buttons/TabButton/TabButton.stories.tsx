e74e53adf943f0c4286d204f1cd695c150ef661b
hwinkr <dnddl8280@naver.com>
2024-08-22 13:55:42 +0900

    feat: 재사용할 수 있는 탭 버튼 컴포넌트 구현
    - 약속 참여자 조회 필터링, 약속 시간 되는/안되는으로 구분에 사용되는 탭 버튼 컴포넌트 구현

---
+++ frontend/src/components/_common/Buttons/TabButton/TabButton.stories.tsx
@@ -0,0 +1,98 @@
+import { css } from '@emotion/react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Text from '@components/_common/Text';
+
+import Check from '@assets/images/attendeeCheck.svg';
+
+import TabButton from '.';
+
+const meta = {
+  title: 'Components/Button/TabButton',
+  component: TabButton,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    isActive: {
+      type: 'boolean',
+    },
+    tabButtonVariants: {
+      control: 'select',
+      options: ['default', 'outlinedFloating'],
+    },
+    onClick: {
+      type: 'function',
+    },
+  },
+} satisfies Meta<typeof TabButton>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    isActive: false,
+    onClick: () => {},
+    children: '버튼',
+  },
+};
+
+export const Playground: Story = {
+  args: {
+    isActive: false,
+    tabButtonVariants: 'default',
+    onClick: () => console.log('hi'),
+    children: <Text typo="captionBold">Tab Button</Text>,
+  },
+};
+
+export const AttendeeFilteringButton: Story = {
+  args: {},
+  render: (args) => (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        row-gap: 1.2rem;
+      `}
+    >
+      <TabButton
+        tabButtonVariants="outlinedFloating"
+        isActive={true}
+        onClick={() => console.log('.')}
+      >
+        <Check width="16" height="16" />
+        페드로
+      </TabButton>
+      <TabButton
+        tabButtonVariants="outlinedFloating"
+        isActive={false}
+        onClick={() => console.log('.')}
+      >
+        해리
+      </TabButton>
+    </div>
+  ),
+};
+
+export const TimeSelectModeButton: Story = {
+  args: {},
+  render: (args) => (
+    <div
+      css={css`
+        display: flex;
+        gap: 1.2rem;
+      `}
+    >
+      <TabButton isActive={true} onClick={() => console.log('.')}>
+        되는
+      </TabButton>
+      <TabButton isActive={false} onClick={() => console.log('.')}>
+        안되는
+      </TabButton>
+    </div>
+  ),
+};
---
+++ frontend/src/components/_common/Buttons/TabButton/TabButton.styles.ts
@@ -0,0 +1,43 @@
+import type { SerializedStyles } from '@emotion/react';
+import { css } from '@emotion/react';
+
+import theme from '@styles/theme';
+
+import type { TabButtonVariants } from './TabButton.types';
+
+type TabButtonStyles = Record<TabButtonVariants, SerializedStyles>;
+
+const ACTIVE_TAB_BUTTON_STYLES: TabButtonStyles = {
+  default: css`
+    color: #33272a;
+    background-color: ${theme.colors.primary};
+  `,
+  outlinedFloating: css`
+    background-color: #ffc2d0;
+    border-radius: 999rem;
+    box-shadow: inset 0 0 0 1px ${theme.colors.primary};
+  `,
+};
+
+const INACTIVE_TAB_BUTTON_STYLES: TabButtonStyles = {
+  default: css`
+    color: #a1a1aa;
+    background-color: e4e4e7;
+  `,
+  outlinedFloating: css`
+    color: #a1a1aa;
+    background-color: e4e4e7;
+    border-radius: 999rem;
+    box-shadow: inset 0 0 0 1px #e4e4e7;
+  `,
+};
+
+export const s_tabButton = (
+  isActive: boolean,
+  tabButtonVariants: 'default' | 'outlinedFloating',
+) => css`
+  outline: 0 solid ${theme.colors.white};
+  ${isActive
+    ? ACTIVE_TAB_BUTTON_STYLES[tabButtonVariants]
+    : INACTIVE_TAB_BUTTON_STYLES[tabButtonVariants]};
+`;
