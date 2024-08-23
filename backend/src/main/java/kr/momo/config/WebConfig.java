820d31065a8f516cfcc19eb80504d7254e4dffc7
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-08-23 09:40:46 +0900

    [BE] 요청 로그에 사용자 정보를 추가 (#298)
    * refactor: IP 주소 정보 삭제
    
    * feat: 요청에 사용자 정보 추가
    
    * fix: 예외 발생시 500응답 수정
    
    * refactor: 패키지 이름 수정
    
    * refactor: 역할에 따라 인터셉터 분
    
    * refactor: userInfo 상수로 추출
    
    * refactor: 인터셉터 등록 순서 명시적으로 지정
    
    * refactor: 인터셉터 이름 수정
    
    * refactor: 응답 로그에도 사용자 정보 추가
    
    * refactor: MDC를 통해서 유저 정보를 가져오도록 수정

--- backend/src/main/java/kr/momo/config/WebConfig.java
+++ backend/src/main/java/kr/momo/config/WebConfig.java
@@ -1,20 +1,33 @@
 package kr.momo.config;
 
 import java.util.List;
+import kr.momo.config.interceptor.JwtInterceptor;
+import kr.momo.config.interceptor.LoggingInterceptor;
 import kr.momo.controller.auth.AuthArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final String BASE_URL = "/api/v1/**";
+
     private final AuthArgumentResolver authArgumentResolver;
+    private final JwtInterceptor jwtInterceptor;
+    private final LoggingInterceptor loggingInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor).addPathPatterns(BASE_URL).order(1);
+        registry.addInterceptor(loggingInterceptor).addPathPatterns(BASE_URL).order(2);
     }
 }
--- backend/src/main/java/kr/momo/config/interceptor/LogGenerator.java
+++ backend/src/main/java/kr/momo/config/interceptor/LogGenerator.java
@@ -1,27 +1,39 @@
-package kr.momo.config.filter;
+package kr.momo.config.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class LogGenerator {
 
     public void logRequest(String traceId, HttpServletRequest request) {
         String httpMethod = request.getMethod();
         String requestURI = request.getRequestURI();
-        String remoteAddr = request.getRemoteAddr();
+        String userInfo = MDC.get(JwtInterceptor.USER_INFO);
 
-        log.info("REQUEST [{}][{} {}][{}]", traceId, httpMethod, requestURI, remoteAddr);
+        log.info("REQUEST [{}][USERID:{}][{} {}]", traceId, userInfo, httpMethod, requestURI);
     }
 
     public void logResponse(String traceId, long duration, HttpServletRequest request, HttpServletResponse response) {
         String httpMethod = request.getMethod();
         String requestURI = request.getRequestURI();
+        String userInfo = MDC.get(JwtInterceptor.USER_INFO);
         int status = response.getStatus();
 
-        log.info("RESPONSE [{}][{} {}][{} ms][Status: {}]", traceId, httpMethod, requestURI, duration, status);
+        log.info(
+                "RESPONSE [{}][USERID:{}][{} {}][{} ms][Status: {}]",
+                traceId,
+                userInfo,
+                httpMethod,
+                requestURI,
+                duration,
+                status
+        );
     }
 }
