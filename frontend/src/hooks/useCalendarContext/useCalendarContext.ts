7055d8d64e1791fe1662809c00b4e751fd8c5b67
hwinkr <dnddl8280@naver.com>
2024-09-15 16:11:25 +0900

    feat: 달력을 구성하는 각각의 컴포넌트들이 Context API로 공유되는 달력 데이터를 뽑아서 사용할 수 있도록 커스텀 훅 구현

---
+++ frontend/src/hooks/useCalendarContext/useCalendarContext.ts
@@ -0,0 +1,13 @@
+import { useContext } from 'react';
+
+import { CalendarContext } from '@components/_common/Calendar';
+
+export const useCalendarContext = () => {
+  const context = useContext(CalendarContext);
+
+  if (!context) {
+    throw new Error('useCalendarContext 커스텀 훅은 캘린더 컴포넌트 내부에서만 사용할 수 있어요^^');
+  }
+
+  return context;
+};
