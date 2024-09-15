98acf0a1a087680963078540d1f846101a95b6a9
hwinkr <dnddl8280@naver.com>
2024-09-15 15:52:08 +0900

    chore: 이전 달력 컴포넌트 & 로직 레거시 폴더로 이동

---
+++ frontend/legacy/Calendar/Calendar.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+
+import Calendar from './index';
+
+const meta = {
+  title: 'Components/Calendar',
+  component: Calendar,
+  tags: ['autodocs'],
+
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    hasDate: {
+      description: '선택된 날짜들',
+      type: 'function',
+      control: {
+        disable: true,
+      },
+    },
+    onDateClick: {
+      description: '선택된 날짜 리스트에 특정 날짜를 추가하거나 제거할 수 있는 함수',
+    },
+  },
+  decorators: [
+    (Story, context) => {
+      const [selectedDates, setSelectedDates] = useState<string[]>([]);
+
+      const hasDate = (date: string) => selectedDates.includes(date);
+
+      const handleDateClick = (date: string) => {
+        setSelectedDates((prevDates) =>
+          hasDate(date) ? prevDates.filter((d) => d !== date) : [...prevDates, date],
+        );
+      };
+
+      return (
+        <Story
+          args={{
+            ...context.args,
+            hasDate,
+            onDateClick: handleDateClick,
+          }}
+        />
+      );
+    },
+  ],
+} satisfies Meta<typeof Calendar>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Playground: Story = {
+  args: {
+    hasDate: () => false,
+    onDateClick: () => {},
+  },
+  render: (args) => {
+    return <Calendar hasDate={args.hasDate} onDateClick={args.onDateClick} />;
+  },
+};
+export default {};
