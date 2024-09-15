2d1e886de417489b79b423c9850f2775b1ba22f2
hwinkr <dnddl8280@naver.com>
2024-09-15 16:02:17 +0900

    feat: 공통 달력 컴포넌트, 일주일 날짜를 표현하는 컴포넌트 구현
    - render props 패턴을 활용하여 useCalendar 컴포넌트가 반환하는 일주일 날짜 데이터를 사용해서 UI를 그리는 컴포넌트를 호출할 수 있도록 구현

---
+++ frontend/src/components/_common/Calendar/Weekdays/index.tsx
@@ -0,0 +1,15 @@
+// import 하지 않으면 스토리북에서 캘린더 컴포넌트가 렌더링 되지 않아 일단 추가(@해리)
+import React from 'react';
+
+import { useCalendarContext } from '@hooks/useCalendarContext/useCalendarContext';
+
+interface WeekDaysProps {
+  render: (weekDays: string[]) => JSX.Element;
+}
+
+export default function WeekDays({ render }: WeekDaysProps) {
+  const { headers } = useCalendarContext();
+  const { weekDays } = headers;
+
+  return <>{render(weekDays)}</>;
+}
