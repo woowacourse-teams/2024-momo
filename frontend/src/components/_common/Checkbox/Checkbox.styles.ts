9a0c3f3eb44004d99314afeeeff828fe2c6934f3
Largopie <jae990320@kakao.com>
2024-09-22 23:34:33 +0900

    feat(Checkbox): 체크박스 공통 컴포넌트 구현

---
+++ frontend/src/components/_common/Checkbox/Checkbox.styles.ts
@@ -0,0 +1,7 @@
+import { css } from '@emotion/react';
+
+export const s_container = css`
+  display: flex;
+  align-items: center;
+  gap: 0.2rem;
+`;
---
+++ frontend/src/components/_common/Checkbox/index.tsx
@@ -0,0 +1,18 @@
+import type { InputHTMLAttributes } from 'react';
+import React from 'react';
+
+import { s_container } from './Checkbox.styles';
+
+interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+  isChecked: boolean;
+  labelText: string;
+}
+
+export default function Checkbox({ isChecked, id, labelText, ...props }: CheckboxProps) {
+  return (
+    <div css={s_container}>
+      <input id={id} checked={isChecked} type="checkbox" {...props} />
+      <label htmlFor={id}>{labelText}</label>
+    </div>
+  );
+}
