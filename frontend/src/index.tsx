19ead22aba932ad3c44a85d24f4d04348ac71a2e
해리(최현웅) <dnddl8280@naver.com>
2024-09-23 22:45:09 +0900

    [ALL] frontend-prod-cd.yml 파일의 detect-change job 수정 (#330)
    * [FE] dev 배포시에도 msw가 실행되었던 문제 해결 (#322)
    
    chore: msw 실행 조건 추가
    
    * [FE] build:dev 스크립트에서 노드 환경을 production으로 수정 (#324)
    
    chore: build:dev 스크립트를 development에서 production으로 수정
    
    * Revert "[FE] build:dev 스크립트에서 노드 환경을 production으로 수정" (#325)
    
    * Revert "[FE] build:dev 스크립트에서 노드 환경을 production으로 수정 (#324)"
    
    This reverts commit 9bbd94efffe61a0367038df7d3d2d210d1378884.
    
    * chore: cross-env 제거
    
    * [FE] build:dev 스크립트에서 노드 환경을 production으로 수정 (#326)
    
    * Revert "Revert "[FE] build:dev 스크립트에서 노드 환경을 production으로 수정" (#325)"
    
    This reverts commit 66fbefc9dbc9c2cae8f3570884f7ba917dcde12c.
    
    * chore: cross-env 제거 + dev & prod 모두 production으로 설정
    
    * [FE] build:dev 스크립트에서 노드 환경을 production으로 수정 (#327)
    
    * Revert "[FE] build:dev 스크립트에서 노드 환경을 production으로 수정 (#326)"
    
    This reverts commit b9cd00952a67fac06d455b44e2f4a7948f4f0742.
    
    * chore: dev모드 빌드 시 production모드로 빌드하도록 수정
    
    * refactor: 불필요한 로직 제거
    
    * chore: 프론트엔드 리소스 프로덕션 환경 배포 yml 파일 수정
    
    ---------
    
    Co-authored-by: 낙타/송재석 <106071687+Largopie@users.noreply.github.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -14,6 +14,7 @@ const enableMocking = async () => {
   if (process.env.NODE_ENV !== 'development') {
     return;
   }
+
   const { worker } = await import('@mocks/browser');
 
   return worker.start();
