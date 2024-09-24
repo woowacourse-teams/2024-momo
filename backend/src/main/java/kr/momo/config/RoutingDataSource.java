6dd3ac70444eb6e408c3efe53d3f85c6d1658f82
ikjo39 <chi4321@naver.com>
2024-09-24 20:09:25 +0900

    feat: `@Transactional` readOnly 설정 별 DB 트랜잭션 분산 로직 추가

---
+++ backend/src/main/java/kr/momo/config/RoutingDataSource.java
@@ -0,0 +1,15 @@
+package kr.momo.config;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class RoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            return DataSourceConfig.REPLICA_SERVER;
+        }
+        return DataSourceConfig.SOURCE_SERVER;
+    }
+}
