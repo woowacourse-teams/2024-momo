52e99961a9c49b63f5362c3c857707fa75f8d9f2
ikjo39 <chi4321@naver.com>
2024-09-27 12:26:09 +0900

    hotfix: replica로 업데이트 쿼리 날아가던 오류 수정

--- backend/src/main/java/kr/momo/config/RoutingDataSource.java
+++ backend/src/main/java/kr/momo/config/RoutingDataSource.java
@@ -1,15 +1,19 @@
 package kr.momo.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 public class RoutingDataSource extends AbstractRoutingDataSource {
 
+    private static final Logger log = LoggerFactory.getLogger(RoutingDataSource.class);
+
     @Override
     protected Object determineCurrentLookupKey() {
-        if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
-            return DataSourceConfig.REPLICA_SERVER;
-        }
-        return DataSourceConfig.SOURCE_SERVER;
+        boolean currentTransactionReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+        String key = currentTransactionReadOnly ? DataSourceConfig.REPLICA_SERVER : DataSourceConfig.SOURCE_SERVER;
+        log.info("Activated Datasource: {}", key);
+        return key;
     }
 }
