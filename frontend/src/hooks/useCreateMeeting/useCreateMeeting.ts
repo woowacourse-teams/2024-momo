ed17243497b68a1b34e82fff4a696ac0812d8bdd
hwinkr <dnddl8280@naver.com>
2024-10-07 11:59:41 +0900

    refactor: 약속 생성 api 함수 추가, 입력 필드 유효성 검증 함수 추가 분리, invalid를 하나의 단어로 사용하도록 수정

--- frontend/src/hooks/useCreateMeeting/useCreateMeeting.ts
+++ frontend/src/hooks/useCreateMeeting/useCreateMeeting.ts
@@ -1,47 +1,68 @@
 import { useState } from 'react';
 
 import useInput from '@hooks/useInput/useInput';
+import { INITIAL_END_TIME, INITIAL_START_TIME } from '@hooks/useTimeRangeDropdown/constants';
 import useTimeRangeDropdown from '@hooks/useTimeRangeDropdown/useTimeRangeDropdown';
 
-import { FIELD_DESCRIPTIONS, INPUT_FIELD_PATTERN } from '@constants/inputFields';
+import { usePostMeetingMutation } from '@stores/servers/meeting/mutation';
+
+import { FIELD_DESCRIPTIONS, INPUT_FIELD_PATTERN, INPUT_RULES } from '@constants/inputFields';
+
+const checkInputInvalid = (value: string, errorMessage: string | null) =>
+  value.length < INPUT_RULES.minimumLength || errorMessage !== null;
 
 const useCreateMeeting = () => {
   const meetingNameInput = useInput({
     pattern: INPUT_FIELD_PATTERN.meetingName,
     errorMessage: FIELD_DESCRIPTIONS.meetingName,
   });
-  const isMeetingNameInvalid =
-    meetingNameInput.errorMessage !== null || meetingNameInput.value.length < 1;
+  const isMeetingNameInvalid = checkInputInvalid(
+    meetingNameInput.value,
+    meetingNameInput.errorMessage,
+  );
 
   const hostNickNameInput = useInput({
     pattern: INPUT_FIELD_PATTERN.nickname,
     errorMessage: FIELD_DESCRIPTIONS.nickname,
   });
-
   const hostPasswordInput = useInput({
     pattern: INPUT_FIELD_PATTERN.password,
     errorMessage: FIELD_DESCRIPTIONS.password,
   });
   const isHostInfoInValid =
-    hostNickNameInput.value.length < 1 ||
-    hostPasswordInput.value.length < 1 ||
-    hostNickNameInput.errorMessage !== null ||
-    hostPasswordInput.errorMessage !== null;
+    checkInputInvalid(hostNickNameInput.value, hostNickNameInput.errorMessage) ||
+    checkInputInvalid(hostPasswordInput.value, hostPasswordInput.errorMessage);
 
   const [selectedDates, setSelectedDates] = useState<string[]>([]);
   const areDatesUnselected = selectedDates.length < 1;
   const hasDate = (date: string) => selectedDates.includes(date);
-
   const handleDateClick = (date: string) => {
     setSelectedDates((prevDates) =>
       hasDate(date) ? prevDates.filter((d) => d !== date) : [...prevDates, date],
     );
   };
 
-  const { startTime, endTime, handleStartTimeChange, handleEndTimeChange } = useTimeRangeDropdown();
+  const meetingTimeInput = useTimeRangeDropdown();
 
-  const isCreateMeetingFormInValid =
+  const isCreateMeetingFormInvalid =
     isMeetingNameInvalid || (isHostInfoInValid && areDatesUnselected);
+
+  const { mutation: postMeetingMutation } = usePostMeetingMutation();
+
+  const handleMeetingCreateButtonClick = () => {
+    postMeetingMutation.mutate({
+      meetingName: meetingNameInput.value,
+      hostName: hostNickNameInput.value,
+      hostPassword: hostPasswordInput.value,
+      availableMeetingDates: selectedDates,
+      meetingStartTime: meetingTimeInput.startTime.value,
+      // 시간상 24시는 존재하지 않기 때문에 백엔드에서 오류가 발생. 따라서 오전 12:00으로 표현하지만, 서버에 00:00으로 전송(@낙타)
+      meetingEndTime:
+        meetingTimeInput.endTime.value === INITIAL_END_TIME
+          ? INITIAL_START_TIME
+          : meetingTimeInput.endTime.value,
+    });
+  };
 
   return {
     meetingNameInput,
@@ -51,13 +72,9 @@ const useCreateMeeting = () => {
     isHostInfoInValid,
     hasDate,
     handleDateClick,
-    meetingTimeInput: {
-      startTime,
-      endTime,
-      handleEndTimeChange,
-      handleStartTimeChange,
-    },
-    isCreateMeetingFormInValid,
+    meetingTimeInput,
+    isCreateMeetingFormInvalid,
+    handleMeetingCreateButtonClick,
   };
 };
 
