b04edfecba383900f268fbcbb833fb12ff5932e1
hwinkr <dnddl8280@naver.com>
2024-10-08 20:49:17 +0900

    feat: 토스트를 감싸는 컴포넌트 구현
    - 토스트 컴포넌트 UI 테스트를 쉽게할 수 있도록, 토스트가 사라질 때 애니메이션을 적용하기 위한 상태를 관리하는 Wrapper 컴포넌트 구현

---
+++ frontend/src/components/_common/Toast/ToastContainer.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+import Toast from '.';
+import type { ToastType } from './Toast.type';
+
+interface ToastContainerProps {
+  duration: number;
+  type: ToastType;
+  message: string;
+}
+
+const TOAST_ANIMATION_DURATION_TIME = 500;
+
+export default function ToastContainer({ duration, type, message }: ToastContainerProps) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  useEffect(() => {
+    const animationTimer = setTimeout(() => {
+      setIsOpen(false);
+    }, duration - TOAST_ANIMATION_DURATION_TIME);
+
+    return () => {
+      clearTimeout(animationTimer);
+    };
+  }, [duration]);
+
+  return <Toast isOpen={isOpen} type={type} message={message} />;
+}
