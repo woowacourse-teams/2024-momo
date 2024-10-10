c37fda5a65748b1548a4a832e676cbe197e7511a
Yoonkyoungme <100656920+Yoonkyoungme@users.noreply.github.com>
2024-10-10 11:38:13 +0900

    design: 요소가 넘쳐서 안 보이는 경우 스크롤 가능하게 설정

--- frontend/src/pages/CreateMeetingPage/components/MeetingDateTime/MeetingDateTime.styles.ts
+++ frontend/src/pages/CreateMeetingPage/components/MeetingDateTime/MeetingDateTime.styles.ts
@@ -1,9 +1,12 @@
 import { css } from '@emotion/react';
 
 export const s_container = css`
+  overflow: scroll;
   display: flex;
   flex-direction: column;
   gap: 2.4rem;
+
+  height: calc(100% - 6rem);
 `;
 
 export const s_dateCandidateSelector = css`
