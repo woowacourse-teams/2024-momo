55315d50b3070eaa11c7c14aa8e294c93d312612
Yoonkyoungme <100656920+Yoonkyoungme@users.noreply.github.com>
2024-10-11 15:58:44 +0900

    feat: MeetingRegisterPage 구현

---
+++ frontend/src/pages/MeetingRegisterPage/MeetingRegisterPage.styles.ts
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+
+export const s_container = css`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const s_pageHeader = css`
+  display: flex;
+  flex-direction: column;
+  row-gap: 1.2rem;
+  margin-bottom: 1.2rem;
+`;
+
+export const s_contentDivider = css`
+  width: calc(100% + 1.6rem * 2);
+  height: 0.4rem;
+  margin: 1.6rem 0 0 -1.6rem;
+  background-color: #f6f6f6;
+`;
