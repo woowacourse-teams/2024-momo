7736d5a9bf04adfb1f980ef4a35f19c23df59863
hwinkr <dnddl8280@naver.com>
2024-10-19 11:41:23 +0900

    feat: floating 스타일, 타입 추가

--- frontend/src/components/_common/Input/index.tsx
+++ frontend/src/components/_common/Input/index.tsx
@@ -2,12 +2,13 @@ import type { InputHTMLAttributes } from 'react';
 
 import { s_input } from './Input.styles';
 
-export type InputVariant = 'default' | 'transparent';
+export type InputVariant = 'default' | 'transparent' | 'floating';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   variant?: InputVariant;
+  isError?: boolean;
 }
 
 export default function Input({ variant = 'default', type = 'text', ...props }: InputProps) {
-  return <input css={s_input[variant]} type={type} {...props} />;
+  return <input css={s_input[variant]} type={type} spellCheck={false} {...props} />;
 }
