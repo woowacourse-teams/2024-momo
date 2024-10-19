e92fa4a7b6a7c9a58998fb647fa8d8dd2bc79055
hwinkr <dnddl8280@naver.com>
2024-10-19 11:51:48 +0900

    refactor: 약속 참여자 로그인 페이지 로직, 스타일 수정
    - FloatingInput으로 수정
    - useAttendeeLogin 커스텀 훅 활용
    - 필드가 페이지 상단에 위치하도록 변경

---
+++ frontend/src/hooks/useAttendeeLogin/useAttendeeLogin.ts
@@ -0,0 +1,63 @@
+import { useContext } from 'react';
+
+import { UuidContext } from '@contexts/UuidProvider';
+
+import useInput from '@hooks/useInput/useInput';
+
+import { usePostLoginMutation } from '@stores/servers/user/mutations';
+
+import { FIELD_ERROR_MESSAGES, INPUT_FIELD_PATTERN } from '@constants/inputFields';
+
+const useAttendeeLogin = () => {
+  const { uuid } = useContext(UuidContext);
+  const { mutate: postLoginMutate } = usePostLoginMutation();
+
+  const attendeeNameInput = useInput({
+    pattern: INPUT_FIELD_PATTERN.nickname,
+    errorMessage: FIELD_ERROR_MESSAGES.nickname,
+  });
+  const isAttendeeNameError = attendeeNameInput.errorMessage !== null;
+  const attendeeNameField = { ...attendeeNameInput, isError: isAttendeeNameError };
+
+  const attendeePasswordInput = useInput({
+    pattern: INPUT_FIELD_PATTERN.password,
+    errorMessage: FIELD_ERROR_MESSAGES.password,
+  });
+  const isAttendeePasswordError = attendeePasswordInput.errorMessage !== null;
+  const attendeePasswordField = { ...attendeePasswordInput, isError: isAttendeePasswordError };
+
+  const isFormValid = () => {
+    const hasLoginFormError = isAttendeeNameError || isAttendeePasswordError;
+
+    if (hasLoginFormError) {
+      return false;
+    }
+
+    const requiredFields = [attendeeNameInput.value, attendeePasswordInput.value];
+    const isAllFieldsFilled = requiredFields.every((field) => field !== '');
+
+    return isAllFieldsFilled;
+  };
+
+  const handleLoginButtonClick = async () => {
+    if (!uuid) {
+      console.error('UUID is missing');
+      return;
+    }
+
+    postLoginMutate({
+      uuid,
+      request: { attendeeName: attendeeNameInput.value, password: attendeePasswordInput.value },
+    });
+  };
+
+  return {
+    attendeeNameField,
+    attendeePasswordField,
+    isFormValid,
+    handleLoginButtonClick,
+    uuid,
+  };
+};
+
+export default useAttendeeLogin;
