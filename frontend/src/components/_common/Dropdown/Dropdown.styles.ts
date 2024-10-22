9e5679830ba183692f7359d3247a075d8267df99
hwinkr <dnddl8280@naver.com>
2024-10-22 17:06:42 +0900

    chore: IOS 기기에서 드롭다운 텍스트가 파란색으로 보이는 문제 해결

--- frontend/src/components/_common/Dropdown/Dropdown.styles.ts
+++ frontend/src/components/_common/Dropdown/Dropdown.styles.ts
@@ -1,8 +1,13 @@
 import { css } from '@emotion/react';
 
+import theme from '@styles/theme';
+
 export const s_dropdown = css`
   width: fit-content;
   height: 2.8rem;
   padding: 0.4rem;
+
+  color: ${theme.colors.black};
+
   border-radius: 0.4rem;
 `;
