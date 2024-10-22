8777484b92b6cbb074d5ce553deeeb1ee6885bbc
hwinkr <dnddl8280@naver.com>
2024-10-22 17:06:50 +0900

    chore: css 선언 순서 수정

--- frontend/src/components/TimeRangeSelector/TimeRangeSelector.styles.ts
+++ frontend/src/components/TimeRangeSelector/TimeRangeSelector.styles.ts
@@ -3,6 +3,6 @@ import { css } from '@emotion/react';
 export const s_dropdownContainer = css`
   display: flex;
   gap: 1.2rem;
-  justify-content: flex-start;
   align-items: center;
+  justify-content: flex-start;
 `;
