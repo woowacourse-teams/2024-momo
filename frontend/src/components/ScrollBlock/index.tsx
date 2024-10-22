af03a3034d94183d960f2fed28064c0bb0e705cf
hwinkr <dnddl8280@naver.com>
2024-10-22 17:06:13 +0900

    chore: 직관적인 함수명을 사용하는 것으로 수정

--- frontend/src/components/ScrollBlock/index.tsx
+++ frontend/src/components/ScrollBlock/index.tsx
@@ -5,15 +5,15 @@ export default function ScrollBlock({ children }: PropsWithChildren) {
   const contentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const handleTouchMove = (e: TouchEvent) => {
+    const preventTouchMove = (e: TouchEvent) => {
       e.preventDefault();
     };
 
     // 터치 이벤트를 사용해서 스크롤을 할 경우, 해당 스크롤을 막는다는 것을 브라우저에게 명시적으로 알려주기 위해서 passive 속성 추가(@해리)
-    document.addEventListener('touchmove', handleTouchMove, { passive: false });
+    document.addEventListener('touchmove', preventTouchMove, { passive: false });
 
     return () => {
-      document.removeEventListener('touchmove', handleTouchMove);
+      document.removeEventListener('touchmove', preventTouchMove);
     };
   }, []);
 
